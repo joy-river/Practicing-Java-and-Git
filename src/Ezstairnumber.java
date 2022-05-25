e1170a3680ce0ab18c7c5c87ced96553354cb12f
Harigaze <dlrkd1122@korea.ac.kr>
2022-05-25 20:33:35 +0900

    백준 10844번 쉬운 계단 수 DP문제
    알고리즘은
    1 -> 0이 맨 앞인 n - 1 계단 수  -> 1이 맨 앞인 n - 2 계단 수 - > 2, 0 n  - 3 계단 수
    대략 이러한 분할 정복.. 바텀 업 방식으로 한자리 수 부터 전부 쌓아가는 형식, 계산상 맨 앞자리가 0일때도 필요하므로 계산은 하되, 마지막 n 자리 수 의 총합에서는 따로 빼서 제대로 된 합을 구함. 나머지 구하는 수는 mod가 아니라 div로 되어있으므로 헷갈리지 말것. 최종 계산에서도 %div를 남발함.

---
+++ src/Ezstairnumber.java
@@ -0,0 +1,35 @@
+import java.io.*;
+
+public class Ezstairnumber {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    static final int div = 1_000_000_000;
+    static int n;
+    static long count =0 ;
+    public static void main(String[] args) throws IOException {
+        n = Integer.parseInt(br.readLine());
+        long[][] d = new long[n][10];
+        for (int i = 0 ; i < 10 ; i++)
+            d[0][i] = 1;
+
+        for (int i = 0; i < n - 1; i ++){
+            for (int j = 0 ; j < 10 ; j++){
+                if (j == 0)
+                    d[i + 1][j + 1] += d[i][j] % div;
+                else if(j == 9)
+                    d[i + 1][j - 1] += d[i][j] % div;
+                else{
+                    d[i + 1][j + 1] += d[i][j] % div;
+                    d[i + 1][j - 1] += d[i][j] % div;
+                }
+            }
+        }
+        for (long i : d[n-1])
+            count += i;
+
+        bw.write(Long.toString((count - d[n - 1][0]) % div));
+        bw.flush();
+        bw.close();
+    }
+
+}
