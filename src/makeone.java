3668936e373efa1ae820f039083ba6450818c43d
Harigaze <dlrkd1122@korea.ac.kr>
2022-05-25 13:49:43 +0900

    백준 1463 1로 만들기 DP 문제
    1을 빼는 케이스를 가장 먼저 고려하고, 2로 나누는 케이스 다음 3 케이스를 나열하는 바텀 업 방식으로 코딩. 고려해볼만한 반례는 642(답 10), 10(답 3)등이 있음. 6으로 나누어떨어질때 답안이 좀 이상해지는것 고려.

---
+++ src/makeone.java
@@ -0,0 +1,22 @@
+import java.io.*;
+
+public class makeone {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+    public static void main(String[] args) throws IOException {
+        int n = Integer.parseInt(br.readLine());
+        int []d = new int [n + 1];
+
+        for (int i = 2 ; i < n + 1 ; i ++){
+            d[i] = d[i-1] + 1;
+            if (i % 2 == 0)
+                d[i] = Math.min(d[i] - 1, d[i / 2]) + 1;
+            if(i % 3 == 0)
+                d[i] = Math.min(d[i] - 1, d[i / 3]) + 1;
+        }
+        bw.write(Integer.toString(d[n]));
+        bw.flush();
+        bw.close();
+    }
+}
