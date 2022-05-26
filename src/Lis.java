56558f58475d8f25c11548881e135ee6275155ee
Harigaze <dlrkd1122@korea.ac.kr>
2022-05-26 22:12:42 +0900

    백준 11053 가장 긴 증가하는 부분 수열(lis) DP 문제
    알고리즘은 간단함.. i 밑에있는 친구 중에 증가 조건을 만족하는 가장 큰 부분수열을 찾아내도록 반복문을 돌림.. 사실 반복문을 왜 돌려도 되는지는 몰르겟음... 분명 시간 초과가 날거라고 생각했는데 시간초과가 나지않아서 그냥 날로 먹은 문제.
    
    사실 은근히 어렵긴 했음 lis의 시작점이 앞이 아니라 뒤에 처박혀있을 수도 있음.
    
    테스트 해볼 만한 예제는
    12
    1 2 3 4 5 4 3 2 1 2 3 4
    7
    8 9 10 1 2 3 4
    등이 있을 듯

---
+++ src/Lis.java
@@ -0,0 +1,33 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Lis {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+
+    public static void main(String[] args) throws IOException {
+        int n = Integer.parseInt(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int[] arr = new int [n] ;
+        int[] dp = new int[n + 1];
+        int max = 1;
+        for (int i = 0 ; i < n ; i ++)
+            arr[i] = Integer.parseInt(st.nextToken());
+        for (int i = 0 ; i < n + 1; i++)
+            dp[i] = 1;
+
+
+        for (int i = 0 ; i< n ; i ++)
+            for (int j = 0 ; j < i; j ++) {
+                if (arr[i] > arr[j])
+                    dp[i] = Math.max(dp[j] + 1, dp[i]);
+                max = Math.max(dp[i], max);
+            }
+
+        bw.write(Integer.toString(max));
+        bw.flush();
+        bw.close();
+    }
+
+}
