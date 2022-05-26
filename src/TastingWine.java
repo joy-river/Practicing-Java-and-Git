0257495fca240e617449466315e34aea5838efb8
Harigaze <dlrkd1122@korea.ac.kr>
2022-05-26 15:57:35 +0900

    백준 2156 포도주 시식 DP 문제
    기본적인 알고리즘은 계단오르기와 거의 비슷함. 차이점은 건너뛰기가 2칸 이상 가능하다는 점. 따라서 2칸 밑에서의 최댓값을 찾아 계산하는 방법을 사용했음. 또한 입력값 (6 / 1 2 3 4 5 6)에서 볼 수 있듯이 0이 아닌 최초의 값을 선택하는 것이 항상 최대합을 구할 수 있는것은 아님. 하지만 3번째 값을 선택하는 경우는 첫번째 값을 부담없이 집을 수 있으므로 중요한 것은 맨 앞과 맨 앞 다음 값일 때만 고려하면 됨. 또한 (3 / 0 0 1) 같은 경우가 있을 수 있으므로 배열 최댓값은 n + 1로 만들었음

---
+++ src/TastingWine.java
@@ -0,0 +1,40 @@
+import java.io.*;
+
+public class TastingWine {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+    public static void main(String[] args) throws IOException {
+        int n = Integer.parseInt(br.readLine());
+        int[] wine = new int [n + 1];
+        int[] d = new int[n + 1];
+        int[] dmax = new int [n + 1];
+        int max = 0;
+        int count = 0;
+        for (int i = 0 ; i < n ; i ++)
+            wine[i] = Integer.parseInt(br.readLine());
+
+        for (int i = 0 ; i < n ; i ++){
+            if(wine[i] != 0 && count < 2){
+                d[i] = wine[i];
+                max = Math.max(max, d[i]);
+                dmax[i] = d[i];
+                for (int j = i + 1; j < n; j++) {
+                    if (j <= i + 2)
+                        d[j] = d[i] + wine[j];
+                    else
+                        d[j] = Math.max(dmax[j-2], dmax[j - 3] + wine[j - 1]) + wine[j];
+                    max = Math.max(max, d[j]);
+                    dmax[j] = Math.max(dmax[j - 1], d[j]);
+
+                }
+                count++;
+            }
+
+        }
+        bw.write(Integer.toString(n == 1 ? wine[0] : max));
+        bw.flush();
+        bw.close();
+    }
+
+}
