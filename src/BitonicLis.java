d825849a4aff377d3822d6d7b73e34379a714cae
Harigaze <dlrkd1122@korea.ac.kr>
2022-05-27 11:23:37 +0900

    백준 11054 가장 긴 바이토닉 부분 수열 DP문제
    간단하게 수열 끝에서부터 시작해서 최장 증가 수열을 구한다음
    수열 처음에서 부터 최장 증가 수열을 구하고
    각 요소마다 합쳐주면 끗
    
    8
    7 8 7 9 5 7 5 2
    같은 반례는 고려할만함.

--- src/BitonicLis.java
+++ src/BitonicLis.java
@@ -1,7 +1,7 @@
 import java.io.*;
 import java.util.StringTokenizer;
 //참고로 부분 수열은 수열의 순서를 유지하는 수열임 ㅇㅇ
-public class Lis {
+public class BitonicLis {
     static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
@@ -11,19 +11,30 @@ public class Lis {
         StringTokenizer st = new StringTokenizer(br.readLine());
         int[] arr = new int [n] ;
         int[] dp = new int[n + 1];
+        int[] dp2 = new int[n+ 1];
         int max = 1;
         for (int i = 0 ; i < n ; i ++)
             arr[i] = Integer.parseInt(st.nextToken());
-        for (int i = 0 ; i < n + 1; i++)
+        for (int i = 0 ; i < n + 1; i++) {
             dp[i] = 1;
+            dp2[i] = 1;
+        }
 
-
-        for (int i = 0 ; i< n ; i ++)
-            for (int j = 0 ; j < i; j ++) {
+        for (int i = n - 1; i >= 0; i --) {
+            for (int j = n-1; j > i; j--) {
                 if (arr[i] > arr[j])
                     dp[i] = Math.max(dp[j] + 1, dp[i]);
-                max = Math.max(dp[i], max);
             }
+        }
+        for (int i = 0 ; i< n ; i ++) {
+            for (int j = 0; j < i; j++) {
+                if (arr[i] > arr[j])
+                    dp2[i] = Math.max(dp2[j] + 1, dp2[i]);
+            }
+            dp[i] += dp2[i] - 1;
+            max = Math.max(dp[i], max);
+        }
+
 
         bw.write(Integer.toString(max));
         bw.flush();
