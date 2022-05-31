5ecd1f16fed8e68e4e4fd5b7fc23c7e26e37fb58
Harigaze <dlrkd1122@korea.ac.kr>
2022-05-31 09:05:09 +0900

    백준 12865 평범한 배낭(냅색) DP 문제
    키포인트는 가방에 들어가는 각 아이템(j)에 대해서 넣었을 때와 안 넣었을때의 밸류를 잘~ 비교 하는것
    여기서 현재 가방의 무게가 i라고 했을때, j 번째 아이템을 담느냐 안 담느냐로 점화식을 구성 가능. 안담았을 경우 dp[i][j]는 j 번째 아이템을 안담은 dp[i][j-1]이 되고, 담았을 경우 현재 무게에서 j 번째 템의 무게를 뺀 dp[i - w][j - 1] 에서 에서 현재 아이템의 가치인 v를 더한 값이 dp[i][j]가 되겠음. 풀이가 상당히 복잡한 편임.
    
    고려해볼 만한 반례
    10 999
    46 306
    60 311
    33 724
    18 342
    57 431
    49 288
    12 686
    89 389
    82 889
    16 289
    
    3 5
    4 2
    3 4
    1 5

---
+++ src/DP/KnapSack.java
@@ -0,0 +1,38 @@
+package DP;
+
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class KnapSack {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+    public static void main(String[] args) throws IOException {
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int k = Integer.parseInt(st.nextToken());
+        int[][] stuff = new int [2][n];
+        int[][] dp = new int [k + 1][n + 1];
+        int max = 0;
+
+        for (int i = 0 ; i < n ; i ++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < 2; j++)
+                stuff[j][i] = Integer.parseInt(st.nextToken());
+        }
+        for (int i = 1; i < k + 1; i ++){
+            for (int j = 1 ; j < n + 1 ; j ++){
+                if(i >= stuff[0][j - 1])
+                    dp[i][j] = Math.max(dp[i][j - 1] , dp[i - stuff[0][j - 1]][j - 1] + stuff[1][j - 1]);
+                else
+                    dp[i][j] = dp[i][j - 1];
+                max = Math.max(dp[i][j], max);
+            }
+        }
+
+        bw.write(Integer.toString(max));
+        bw.flush();
+        bw.close();
+    }
+
+}
