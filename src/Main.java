76cdb4a036f0df04bc84f42f7cf2bf98f063787d
Harigaze <dlrkd1122@korea.ac.kr>
2022-06-04 21:10:34 +0900

    백준 1541 잃어버린 괄호
    하는 중

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,5 @@
 import java.io.*;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Comparator;
-import java.util.StringTokenizer;
+import java.util.*;
 
 
 public class Main {
@@ -10,22 +7,12 @@ public class Main {
     static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
     public static void main(String[] args) throws IOException {
-        StringTokenizer st;
-        int n = Integer.parseInt(br.readLine());
-        int count = 0;
-        int [][] time = new int [n][2];
-
-        for (int i = 0 ; i < n ; i ++){
-            st = new StringTokenizer(br.readLine());
-            time[i][0] = Integer.parseInt(st.nextToken());
-            time[i][1] = Integer.parseInt(st.nextToken());
-        }
-
-        Arrays.sort(time, (o1, o2) -> o1[1] == o2[1] ? Integer.compare(o1[0], o2[0]) : Integer.compare(o1[1], o2[1]));
+       String[] cal = br.readLine().split("[+\\-]");
+       int sum = 0;
 
 
 
-        bw.write(Integer.toString(count));
+        bw.write(Long.toString(sum));
         bw.flush();
         bw.close();
     }
