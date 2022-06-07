ff89f73f716053a0c109b1dcbac937466b9d8911
Harigaze <dlrkd1122@korea.ac.kr>
2022-06-07 17:05:07 +0900

    백준 17298 오큰수 스택 문제
    하는 중

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,7 @@
 import java.io.*;
 import java.util.*;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 
 public class Main {
@@ -7,15 +9,21 @@ public class Main {
     static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
     public static void main(String[] args) throws IOException {
-       String[] cal = br.readLine().split("[+\\-]");
-       int sum = 0;
+        Stack<Integer> stack = new Stack<>();
+        int n = Integer.parseInt(br.readLine());
+        String[] num = br.readLine().split(" ");
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = 0 ;i < n; i ++)
+            stack.push(Integer.parseInt(num[i]));
+
+        for (int i = 0 ; i < n ; i ++)
 
 
-
-        bw.write(Long.toString(sum));
         bw.flush();
         bw.close();
     }
 
 }
+
 
