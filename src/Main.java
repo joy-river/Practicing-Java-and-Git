082c959444ffc9a3c202126afc5e413cd5a4f1a0
Harigaze <dlrkd1122@korea.ac.kr>
2022-06-08 17:19:20 +0900

    백준 17298 오큰수 스택 문제
    하는 중 38 -> 44 퍼까지 왔다.. 그래도 시간초과 뜨기는 한다..

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,5 @@
 import java.io.*;
 import java.util.*;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
-
 
 public class Main {
     static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -12,14 +9,32 @@ public class Main {
         Stack<Integer> stack = new Stack<>();
         int n = Integer.parseInt(br.readLine());
         String[] num = br.readLine().split(" ");
-        StringBuilder sb = new StringBuilder();
-
-        for (int i = 0 ;i < n; i ++)
-            stack.push(Integer.parseInt(num[i]));
+        ArrayList<Integer> valid = new ArrayList<>();
+        int [] nge = new int[n + 1];
+        int last, pop, recent = 0, max = 0;
 
         for (int i = 0 ; i < n ; i ++)
+            stack.push(Integer.parseInt(num[i]));
 
+        for (last = n; last > 0; last --){
+            pop = stack.pop();
+            nge[last] = -1;
+            if(pop <= max) {
+                if (recent > pop) {
+                    valid.add(nge[last] = recent);
+                }
+                else
+                    for (int i = valid.size() - 1; i >= 0; i--)
+                        if (valid.get(i) > pop) {
+                            valid.add(nge[last] = valid.get(i));
+                            break;
+                        }
+            }
+            max = Math.max(recent = pop, max);
+        }
 
+        for (int i = 1; i <=n ; i++)
+            bw.write(nge[i] + " ");
         bw.flush();
         bw.close();
     }
