89202445fcab3d010f0e51c6b199635dc04313b5
Harigaze <dlrkd1122@korea.ac.kr>
2022-06-10 17:20:18 +0900

    백준 1966 프린터큐 하는중
    중요도 카운팅하면 간단할듯

--- src/Main.java
+++ src/Main.java
@@ -6,39 +6,14 @@ public class Main {
     static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
     public static void main(String[] args) throws IOException {
-        Stack<Integer> stack = new Stack<>();
+        Queue<Integer> que = new LinkedList<>();
         int n = Integer.parseInt(br.readLine());
-        String[] num = br.readLine().split(" ");
-        ArrayList<Integer> valid = new ArrayList<>();
-        int [] nge = new int[n + 1];
-        int last, pop, recent = 0, max = 0;
+        for (int i = 0 ; i < n ; i ++){
 
-        for (int i = 0 ; i < n ; i ++)
-            stack.push(Integer.parseInt(num[i]));
-
-        for (last = n; last > 0; last --){
-            pop = stack.pop();
-            nge[last] = -1;
-            if(pop <= max) {
-                if (recent > pop) {
-                    valid.add(nge[last] = recent);
-                }
-                else
-                    for (int i = valid.size() - 1; i >= 0; i--)
-                        if (valid.get(i) > pop) {
-                            valid.add(nge[last] = valid.get(i));
-                            break;
-                        }
-            }
-            max = Math.max(recent = pop, max);
         }
 
-        for (int i = 1; i <=n ; i++)
-            bw.write(nge[i] + " ");
         bw.flush();
         bw.close();
     }
 
 }
-
-
