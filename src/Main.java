4ef4686655f7db7c1f3418efbdac532953ae13af
Harigaze <dlrkd1122@korea.ac.kr>
2022-06-13 17:14:19 +0900

    백준 5430 AC 큐 문제하는 중
    스트링빌더 + 숫자 입력받는거 고치면 될듯. 패턴 매치는 포기함 (정규식을 못짜겠음 능지이슈)

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,52 @@
 import java.io.*;
 import java.util.*;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Main {
     static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
     public static void main(String[] args) throws IOException {
-        Queue<Integer> que = new LinkedList<>();
-        int n = Integer.parseInt(br.readLine());
-        for (int i = 0 ; i < n ; i ++){
+        Deque<Integer> deq = new LinkedList<>();
+        int n = Integer.parseInt(br.readLine()), k;
+        boolean rev, error;
+        StringBuilder sb = new StringBuilder();
+        String [] func, num;
 
+        for (int i = 0 ; i < n ; i ++){
+            rev = false;
+            error = false;
+            func = br.readLine().split("");
+            k = Integer.parseInt(br.readLine());
+            num = br.readLine().split("");
+            while(k != 0)
+                deq.add(Integer.parseInt(num[2 * k-- - 1]));
+            for (String s : func) {
+                if (s.equals("R"))
+                    rev = !rev;
+                else if (deq.isEmpty()) {
+                    error = true;
+                    break;
+                }
+                else if (rev) deq.removeLast();
+                else deq.removeFirst();
+            }
+            if(error)
+                sb.append("error" + "\n");
+            else{
+                sb.append("[");
+                if(rev)
+                    while(!deq.isEmpty()) sb.append(deq.pollLast() + ",");
+                else
+                    while(!deq.isEmpty()) sb.append(deq.pollFirst() + ",");
+                sb.replace(sb.length() - 1, sb.length() - 1, "]" + "\n");
+            }
         }
 
+
+
+        bw.write(sb.toString());
         bw.flush();
         bw.close();
     }
