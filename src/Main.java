afa9b756972fd3c310f056544bc427a2b22f7dc6
Harigaze <dlrkd1122@korea.ac.kr>
2022-06-18 21:41:59 +0900

    백준 1992번 쿼드트리 분할정복 문제
    전에 했던 색종이 문제에 스트링빌더로 압축할 예정

--- src/Main.java
+++ src/Main.java
@@ -4,55 +4,55 @@ import java.util.*;
 public class Main {
     static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-    static int zero = 0, one = 0;
+    static int n;
+    static StringBuilder sb = new StringBuilder();
     public static void main(String[] args) throws IOException {
-
-        int n = Integer.parseInt(br.readLine());
-        int[][] table = new int[n][n];
-        String[] input;
+        n = Integer.parseInt(br.readLine());
+        int [][] table = new int[n][n];
+        String [] line;
 
         for (int i = 0 ; i < n ; i ++){
-            input = br.readLine().split(" ");
-            for (int j = 0 ;j < n; j++)
-                table[i][j] = Integer.parseInt(input[j]);
+            line = br.readLine().split("");
+            for (int j = 0 ; j < n ; j++)
+                table[i][j] = Integer.parseInt(line[j]);
         }
-        counting(table, 0 , 0 ,n);
-        bw.write(zero +"\n" + one);
+
+
         bw.flush();
         bw.close();
     }
-    static int counting (int[][]table,int i, int j, int n){
+    static int counting (int[][]table,int i, int j, int a){
         int temp;
         int temp0 = 0 , temp1 = 0;
-        if(n == 1) {
-            if(table[i][j] == 1)
-                one++;
-            else
-                zero++;
+        if(a == 1)
             return table[i][j];
-        }
         else{
             for (int k = 0 ; k < 2; k++)
                 for (int l = 0 ; l < 2; l++) {
-                    temp  = counting(table, i + k * (n / 2), j + l * (n / 2), n / 2);
-                    if (temp == 0)
-                          temp0++;
-                    else if (temp == 1)
+                    temp  = counting(table, i + k * (a / 2), j + l * (a / 2), a / 2);
+                    if (temp == 0) {
+                        temp0++;
+                    }
+                    else if (temp == 1) {
                         temp1++;
-                    else // return -1일때;;;;;;;;;;;
-                        break;
+                    }
                 }
-            if(temp0 == 4) {
-                zero -= 3;
-                 return 0;
+            if(temp0 == 4 && a != n) {
+                return 0;
+            }
+            else if (temp1 == 4 && a != n){
+                return 1;
+            }
+            else if(temp0 == 4) {
+                return 0;
             }
             else if (temp1 == 4){
-                one -= 3;
                 return 1;
             }
             else
                 return -1;
         }
     }
+
 
 }
