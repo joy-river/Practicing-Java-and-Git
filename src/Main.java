2d25776229dfe9aca85d7cb7f52d8d2dfe14c44b
Harigaze <dlrkd1122@korea.ac.kr>
2022-06-23 17:16:27 +0900

    백준 11401번 이항 계수 3 분할 정복 문제
    푸는 중,
    페르마의 소정리
    모듈러 역원?
    
    하는중 B의 mod -2 승 구하면 끝임 ㅅㄱ

--- src/Main.java
+++ src/Main.java
@@ -4,13 +4,33 @@ import java.io.*;
 public class Main {
     static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-    static Long mod = (long)(1e9+7);
+    static Long mod = 1000000007L;
     public static void main(String[] args) throws IOException {
         String[] input = br.readLine().split(" ");
-
+        Long n = Long.parseLong(input[0]);
+        Long r = Long.parseLong(input[1]);
 
 
         bw.flush();
         bw.close();
+    }
+    static long A (long n) {
+        Long result = 1L;
+        while(n != 1){
+            result *= n--;
+            result %= mod;
+        }
+        return result;
+    }
+    static long B (long n, long r){
+        long result = 1L;
+        while(r != 0){
+            result *= r;
+            result %= mod;
+            result *= (n - r);
+            result %= mod;
+            r--;
+        }
+        return result;
     }
 }
