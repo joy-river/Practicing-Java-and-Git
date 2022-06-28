36a75e87dccc8a7123905f3c45f8330960e41f4e
Harigaze <dlrkd1122@korea.ac.kr>
2022-06-28 09:04:08 +0900

    백준 11401번 이항 계수 3 분할 정복 문제
    페르마의 소정리를 이용해서 모듈러 곱셈 역원을 구한다? 대충 그런 문제. 결론은 이항계수를 구하는 공식을 약간 비틀어서 MOD 값 -2 승을 구하는 식으로 문제를 풀 수 있게 만듦 == 거듭제곱으로 문제를 풀 수 있음.
    
    N == K 일때 (K == 0) 케이스만 따로 떼내면 편함.

---
+++ src/divandconq/BinomiaCoefficient3.java
@@ -0,0 +1,46 @@
+package divandconq;
+
+import java.io.*;
+
+
+public class BinomiaCoefficient3 {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    static Long mod = 1000000007L;
+    public static void main(String[] args) throws IOException {
+        String[] input = br.readLine().split(" ");
+        if(Long.parseLong(input[1]) == 0L || input[0].equals(input[1]))
+            bw.write(Long.toString(1L));
+        else {
+            Long a = A(Long.parseLong(input[0]));
+            Long b = B(Long.parseLong(input[0]), Long.parseLong(input[1]));
+
+            bw.write(Long.toString(a * mult(mod - 2, b, 1L) % mod));
+        }
+        bw.flush();
+        bw.close();
+    }
+    static long A (long n) {
+        Long result = 1L;
+        while(n != 1){
+            result *= n--;
+            result %= mod;
+        }
+        return result;
+    }
+    static long B (long n, long r){
+        long result = A(n - r);
+        result *= A(r);
+        return result % mod;
+    }
+    static Long mult (Long n ,Long num, Long result){
+        Long temp;
+        if (n != 1){
+            temp = mult(n / 2, num, result);
+            result = n % 2 == 0 ? (temp * temp % mod) : ((temp * temp % mod) * num) % mod;
+            return result;
+        }
+        else
+            return num;
+    }
+}
