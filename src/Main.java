614ca3bf913c59e0e87d2111346842a05e98e8d8
Harigaze <dlrkd1122@korea.ac.kr>
2022-06-28 17:11:12 +0900

    행렬 곱셈
    스트라센 알고리즘을 쓰느냐 그냥 행렬 곱으로 푸느냐

--- src/Main.java
+++ src/Main.java
@@ -1,41 +1,18 @@
 import java.io.*;
+import java.util.StringTokenizer;
 
 
 public class Main {
     static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-    static Long mod = 1000000007L;
     public static void main(String[] args) throws IOException {
         String[] input = br.readLine().split(" ");
-        Long a = A(Long.parseLong(input[0])) % mod;
-        Long b = B(Long.parseLong(input[0]), Long.parseLong(input[1])) % mod;
+        StringTokenizer st = new StringTokenizer(br.readLine());
 
-        bw.write(Long.toString(a * mult(mod -2, b, 1L) % mod));
+        input = br.readLine().split(" ");
+        String[][] b = new String[Integer.parseInt(input[0])][];
+        for (int i = 0 ; i < Integer.parseInt(input[0]); i++)
+            b[i] = br.readLine().split(" ");
 
-        bw.flush();
-        bw.close();
-    }
-    static long A (long n) {
-        Long result = 1L;
-        while(n != 1){
-            result *= n--;
-            result %= mod;
-        }
-        return result;
-    }
-    static long B (long n, long r){
-        long result = A(n - r);
-        result *= (A(r)) % mod;
-        return result % mod;
-    }
-    static Long mult (Long n ,Long num, Long result){
-        Long temp;
-        if (n != 1){
-            temp = mult(n / 2, num, result);
-            result = n % 2 == 0 ? (temp * temp % mod) : ((temp * temp % mod) * num) % mod;
-            return result;
-        }
-        else
-            return num;
     }
 }
