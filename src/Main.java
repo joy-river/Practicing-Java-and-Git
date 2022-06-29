cca3b228c58cb62de0bc3491a54811fad2a865ea
Harigaze <dlrkd1122@korea.ac.kr>
2022-06-29 17:14:10 +0900

    행렬 곱셈
    스트라센 알고리즘을 쓰느냐 그냥 행렬 곱으로 푸느냐

--- src/Main.java
+++ src/Main.java
@@ -6,13 +6,31 @@ public class Main {
     static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
     public static void main(String[] args) throws IOException {
-        String[] input = br.readLine().split(" ");
         StringTokenizer st = new StringTokenizer(br.readLine());
-
-        input = br.readLine().split(" ");
-        String[][] b = new String[Integer.parseInt(input[0])][];
-        for (int i = 0 ; i < Integer.parseInt(input[0]); i++)
+        StringBuilder sb = new StringBuilder();
+        int temp = 0;
+        int [] input = {Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken())};
+        String [][] a = new String[input[0]][];
+        for (int i = 0 ; i < input[0]; i ++)
+            a[i] = br.readLine().split(" ");
+        st = new StringTokenizer(br.readLine());
+        int [] input2 = {Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken())};
+        String [][] b = new String[input[0]][];
+        for (int i = 0 ; i < input2[0]; i++)
             b[i] = br.readLine().split(" ");
+
+        for (int i = 0 ; i < input[0]; i ++){
+            for (int s = 0 ; s < input2[0]; s++) {
+                for (int j = 0; j < input[1]; j++) {
+                    temp += Integer.parseInt(a[i][j])  * Integer.parseInt(b[s][j]);
+                }
+            }
+        }
+
+
+
+
+
 
     }
 }
