3324e29ea58fbc82c92e1640f31328fcdf9f3dec
Harigaze <dlrkd1122@korea.ac.kr>
2022-07-01 10:46:12 +0900

    백준 2740 행렬 곱셈 분할 정복 문제
    슈트라센을 쓸까 하다가 말았음 사실 그래서 분할정복이 아님;; 그냥 단순히 곱셈으로 했다 이말이야.

---
+++ src/divandconq/MatrixMulty.java
@@ -0,0 +1,38 @@
+package divandconq;
+
+import java.io.*;
+import java.util.StringTokenizer;
+
+
+public class MatrixMulty {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    public static void main(String[] args) throws IOException {
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        StringBuilder sb = new StringBuilder();
+        int temp = 0;
+        int [] input = {Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken())};
+        String [][] a = new String[input[0]][];
+        for (int i = 0 ; i < input[0]; i ++)
+            a[i] = br.readLine().split(" ");
+        st = new StringTokenizer(br.readLine());
+        int [] input2 = {Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken())};
+        String [][] b = new String[input2[0]][];
+        for (int i = 0 ; i < input2[0]; i++)
+            b[i] = br.readLine().split(" ");
+
+        for (int i = 0 ; i < input[0]; i ++){
+            for (int s = 0 ; s < input2[1]; s++) {
+                temp = 0;
+                for (int j = 0; j < input[1]; j++)
+                    temp += Integer.parseInt(a[i][j])  * Integer.parseInt(b[j][s]);
+                sb.append(temp + " ");
+            }
+            sb.append("\n");
+        }
+
+        bw.write(sb.toString());
+        bw.flush();
+        bw.close();
+    }
+}
