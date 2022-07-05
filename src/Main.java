66090f13f13138ae74d39a32397e3aa522a61106
Harigaze <dlrkd1122@korea.ac.kr>
2022-07-05 17:10:11 +0900

    개같이 히스토그램 히스테리 생길것같애ㅐㅐㅐ!

--- src/Main.java
+++ src/Main.java
@@ -1,58 +1,40 @@
 import java.io.*;
-import java.util.ArrayList;
 import java.util.StringTokenizer;
-
 
 public class Main {
     static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-    static final int mod = 1000;
-    static matrix save;
-    static StringBuilder sb = new StringBuilder();
+    static long[] input;
+    static long output = 0;
     public static void main(String[] args) throws IOException {
         StringTokenizer st = new StringTokenizer(br.readLine());
-        int size = Integer.parseInt(st.nextToken());
-        long n = Long.parseLong(st.nextToken());
-        int[][] mat = new int[size][size];
-        for (int i = 0 ; i < size; i++){
-            st = new StringTokenizer(br.readLine());
-            for (int j = 0 ; j < size; j++)
-                mat[i][j] = Integer.parseInt(st.nextToken());
+        int n;
+        while(true){
+            n = Integer.parseInt(st.nextToken());
+            if(n == 0)
+                break;
+            else{
+                input = new long[n];
+                for(int i  = 0 ; i < n ; i ++)
+                    input[i] = Long.parseLong(st.nextToken());
+            }
         }
-        save = new matrix(mat);
-        bw.write(Matexp(size , n , save).toString());
+
+
         bw.flush();
         bw.close();
     }
-    static matrix Matexp (int size , long n,  matrix mat){
-        matrix temp;
-        if (n != 1){
-            temp = Matexp(size , n /2 , mat);
-            mat = n % 2L == 0 ? mult(temp, temp, size) :  mult(mult(temp, temp, size), save, size);
-            return mat;
-        }
-        else
-            return save;
+    static long hist(int start, int end, int n){
+        long temp = 0;
+        for (int i  = start; i < end; i++){
+            if(input[i] > input[i + 1]){
 
-    }
-    static matrix mult (matrix a, matrix b , int size){
-        int temp;
-        matrix c = new matrix(new int [size][size]);
-        for (int i = 0 ; i < size; i ++){
-            for (int s = 0 ; s < size; s++) {
-                temp = 0;
-                for (int j = 0; j < size; j++)
-                    temp += (a.mat[i][j]  * b.mat[j][s]) % mod;
-                c.mat[i][s] = temp % mod;
-            }
+            }else
+                temp += input[start];
+
         }
-        return c;
     }
+
 }
-class matrix {
-    public matrix(int[][] mat) {
-        this.mat = mat;
-    }
-    int[][] mat;
-}
+
 
