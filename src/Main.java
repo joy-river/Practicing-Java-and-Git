18a3d7a5510baa1b55e06012eb3fcce02b294e22
Harigaze <dlrkd1122@korea.ac.kr>
2022-07-11 14:22:49 +0900

    I tried..

--- src/Main.java
+++ src/Main.java
@@ -5,25 +5,22 @@ public class Main {
     static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
     static long[] input;
-    static long[] hist;
     static long output = 0;
+
     public static void main(String[] args) throws IOException {
         StringTokenizer st = new StringTokenizer(br.readLine());
         int n;
-        while(true){
+        while (true) {
             n = Integer.parseInt(st.nextToken());
-            if(n == 0)
+            if (n == 0)
                 break;
-            else{
+            else {
                 input = new long[n];
-                hist = new long[n];
-                for(int i  = 0 ; i < n ; i ++) {
+                for (int i = 0; i < n; i++)
                     input[i] = Long.parseLong(st.nextToken());
-                    hist[i] = -1L;
-                }
             }
             output = 0;
-            hist (0, n - 1);
+            hist(0, n - 1);
             bw.write(output + "\n");
             st = new StringTokenizer(br.readLine());
         }
@@ -31,34 +28,29 @@ public class Main {
         bw.flush();
         bw.close();
     }
-    static void hist(int start, int end){
-        long temp = input[start];
+
+    static void hist(int start, int end) {
         long min = input[start];
-        if (start == end) {
-            hist[start] = temp;
-            output = Math.max(temp, output);
-        }
-        else {
-            for (int i = start + 1; i <= end; i++) {
-                if(input[i] >= min){
-                    if(hist[i] == -1)
-                        hist(i , end);
-                    temp += min;
-                }
-                else{
-                    min = input[i];
-                    temp = (i + 1 - start) * min;
-                    if(hist[i] == -1)
-                        hist(i ,end);
-                    }
-                output = Math.max(temp, output);
-                hist[start] = Math.max(temp, hist[start]);
-                }
-            }
+        int spot = start;
 
-
-        }
+        for (int i = start + 1 ; i <= end ; i++)
+           if(input[i] < min){
+               min = input[i];
+               spot = i;
+           }
+       output = Math.max(min * (end - start + 1), output);
+       if(start != spot) hist (start , spot -1);
+       if(end != spot) hist (spot + 1, end);
     }
+}
 
+class element {
+    int index;
+    public element(int index, long val) {
+        this.index = index;
+        this.val = val;
+    }
+    long val;
+}
 
 
