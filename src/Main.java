146f8014b2a58ba97ab9502d2f8beebed5a9b132
Harigaze <dlrkd1122@korea.ac.kr>
2022-07-11 17:14:48 +0900

    I tried..

--- src/Main.java
+++ src/Main.java
@@ -5,6 +5,7 @@ public class Main {
     static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
     static long[] input;
+    static node[] seg;
     static long output = 0;
 
     public static void main(String[] args) throws IOException {
@@ -16,10 +17,12 @@ public class Main {
                 break;
             else {
                 input = new long[n];
+                seg = new node[2 * n];
                 for (int i = 0; i < n; i++)
                     input[i] = Long.parseLong(st.nextToken());
             }
             output = 0;
+            inseg(0, 0, n - 1);
             hist(0, n - 1);
             bw.write(output + "\n");
             st = new StringTokenizer(br.readLine());
@@ -28,6 +31,15 @@ public class Main {
         bw.flush();
         bw.close();
     }
+    static node inseg (int index, int start, int end){
+        if(start == end)
+            seg[index] = new node(start, end, input[start]);
+        else
+            seg[index] = new node(start, end, Math.min(inseg(2 * index , start, end / 2).val, inseg(2 * index + 1 , end / 2 + 1  , end).val ));
+        return seg[index];
+    }
+
+    static node find()
 
     static void hist(int start, int end) {
         long min = input[start];
@@ -44,10 +56,12 @@ public class Main {
     }
 }
 
-class element {
-    int index;
-    public element(int index, long val) {
-        this.index = index;
+class node {
+    int start;
+    int end;
+    public node(int start, int end, long val) {
+        this.start = start;
+        this.end = end;
         this.val = val;
     }
     long val;
