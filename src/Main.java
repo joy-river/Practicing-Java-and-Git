2d2f1615c2459415e774e1199f1574247625486c
Harigaze <dlrkd1122@korea.ac.kr>
2022-07-16 21:58:29 +0900

    i tried

--- src/Main.java
+++ src/Main.java
@@ -4,16 +4,16 @@ import java.util.*;
 public class Main {
     static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-    static List<Integer> wire = new ArrayList<>();
+    static List<Integer> wifi = new ArrayList<>();
     static int n;
     public static void main(String[] args) throws IOException {
        StringTokenizer st = new StringTokenizer(br.readLine());
        int k = Integer.parseInt(st.nextToken());
        n = Integer.parseInt(st.nextToken());
        for (int i = 0 ; i < k ; i ++)
-           wire.add(Integer.parseInt(br.readLine()));
-       Collections.sort(wire);
-        cut(0 , wire.get(k - 1));
+           wifi.add(Integer.parseInt(br.readLine()));
+       Collections.sort(wifi);
+        setWifi(0, wifi.get(k));
         bw.flush();
         bw.close();
     }
