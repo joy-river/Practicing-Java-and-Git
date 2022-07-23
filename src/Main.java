f4eb21b220725690aeaaac64965eea8c8b210c39
Harigaze <dlrkd1122@korea.ac.kr>
2022-07-23 19:36:21 +0900

    i tried

--- src/Main.java
+++ src/Main.java
@@ -6,28 +6,24 @@ public class Main {
     static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
     static long output;
     static long n;
+
     public static void main(String[] args) throws IOException {
          n = Long.parseLong(br.readLine());
        long k = Long.parseLong(br.readLine());
-
-        knum(1, 2 * n - 1, k);
+        knum(1, n, k);
         bw.write(Long.toString(output));
        bw.flush();
        bw.close();
     }
     static void knum (long start, long end, long k){
         long mid = (start + end) / 2;
+        long arr = ( count(mid, mid);
+
+        if(arr > k)
 
     }
-    static long count(long start, long end){
-        long count = 0;
-        if(end >= n) {
-            count +=
-            end
-        }
-        start = Math.abs(start - n);
+    static long count(long i) {
 
-        for (int i = 0 ; i < )
     }
 }
 
