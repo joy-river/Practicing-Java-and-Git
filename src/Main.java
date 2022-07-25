30d0bf3b4c092b5a209733cdf7f8731c1f372eb0
Harigaze <dlrkd1122@korea.ac.kr>
2022-07-25 17:22:04 +0900

    I tried..

--- src/Main.java
+++ src/Main.java
@@ -4,24 +4,29 @@ import java.util.*;
 public class Main {
     static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-    static long output;
-    static long n;
 
     public static void main(String[] args) throws IOException {
-         n = Long.parseLong(br.readLine());
-       long k = Long.parseLong(br.readLine());
-        knum(n * n ,k);
-        bw.write(Long.toString(output));
-       bw.flush();
-       bw.close();
-    }
-    static void knum (long num,long k){
-        long mid = num / 2;
+        int n = Integer.parseInt(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int[] arr = new int[n];
+        int[] dp = new int[n + 1];
+        int max = 1;
+        for (int i = 0; i < n; i++)
+            arr[i] = Integer.parseInt(st.nextToken());
+        for (int i = 0; i < n + 1; i++)
+            dp[i] = 1;
 
 
-    }
-    static long count(long i) {
+        for (int i = 0; i < n; i++)
+            for (int j = 0; j < i; j++) {
+                if (arr[i] > arr[j])
+                    dp[i] = Math.max(dp[j] + 1, dp[i]);
+                max = Math.max(dp[i], max);
+            }
 
+        bw.write(Integer.toString(max));
+        bw.flush();
+        bw.close();
     }
 }
 
