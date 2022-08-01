cecae845961c53893d3df889fd22dc2820c8803c
Harigaze <dlrkd1122@korea.ac.kr>
2022-08-01 17:08:21 +0900

    백준 1927번 최소 힙 우선순위 큐 문제
    최대 힙을 직접구현하면서 개 똥꼬쇼를 했으니 최소 힙은 자바 라이브러리로 날먹함.

---
+++ src/PriorityQueue/MinHeap.java
@@ -0,0 +1,30 @@
+package PriorityQueue;
+
+import java.io.*;
+import java.util.*;
+
+public class MinHeap {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+    public static void main(String[] args) throws IOException {
+        PriorityQueue<Integer> min = new PriorityQueue<>();
+        int n = Integer.parseInt(br.readLine());
+        int temp;
+        for (int i =  0 ;  i < n; i++){
+            temp = Integer.parseInt(br.readLine());
+            if(temp == 0){
+                bw.write(min.isEmpty() ? "0\n" : min.poll() + "\n");
+            }
+            else
+                min.add(temp);
+
+        }
+        bw.flush();
+        bw.close();
+    }
+
+
+}
+
+
