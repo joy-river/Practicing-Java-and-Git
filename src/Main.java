56ba955bc31cae22ae1bc862c819c37695f1ff0f
Harigaze <dlrkd1122@korea.ac.kr>
2022-08-01 17:07:58 +0900

    I tried..

--- src/Main.java
+++ src/Main.java
@@ -6,17 +6,16 @@ public class Main {
     static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
     public static void main(String[] args) throws IOException {
-        PriorityQueue<Integer> min = new PriorityQueue<>();
-        int n = Integer.parseInt(br.readLine());
-        int temp;
-        for (int i =  0 ;  i < n; i++){
-            temp = Integer.parseInt(br.readLine());
-            if(temp == 0){
-                bw.write(min.isEmpty() ? "0\n" : min.poll() + "\n");
-            }
-            else
-                min.add(temp);
+        PriorityQueue<Integer> abs = new PriorityQueue<>(Collections.reverseOrder((o1,o2) -> Math.abs(o1) > Math.abs(o2) ?  o2 : o1));
+        int n = Integer.parseInt(br.readLine()), temp;
 
+        for (int i = 0; i < n ;i++){
+            temp =Integer.parseInt(br.readLine());
+
+            if(temp == 0)
+                bw.write(abs.isEmpty()? "0\n" : abs.poll() +"\n");
+            else
+                abs.offer(temp);
         }
         bw.flush();
         bw.close();
