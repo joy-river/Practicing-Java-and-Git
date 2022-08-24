0f913efb7360d4fe8a3bbcdeeeaf9bfb3b3c1e87
Harigaze <dlrkd1122@korea.ac.kr>
2022-08-24 17:18:39 +0900

    I tried..

--- src/Main.java
+++ src/Main.java
@@ -4,34 +4,30 @@ import java.util.*;
 public class Main {
     static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-    static int count;
-    public static void main(String[] args) throws IOException {
-        StringBuilder sb = new StringBuilder();
-        int chan = Integer.parseInt(br.readLine());
-        int beeg = (int) Math.log10(chan);
-        int n = Integer.parseInt(br.readLine());
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        boolean[] able = new boolean[10];
 
-        count = chan > 100 ? chan - 100 : 100 - chan;
-        for (int i = 0 ; i < n ; i ++)
-            able[Integer.parseInt(st.nextToken())] = true;
+    public static void main(String[] args) throws IOException {
+        HashSet<Integer> set = new HashSet<>();
+        HashSet<Integer> allset = new HashSet<>();
+        StringTokenizer st;
+        String input;
+        int n = Integer.parseInt(br.readLine());
+        for (int i = 0 ; i < n ; i ++) {
+            st = new StringTokenizer(br.readLine());
+            input = st.nextToken();
+            switch (input){
+                case "all" : set.addAll(o1 ->  if(o1 < 1))
+                case "add" : set.add(Integer.parseInt(st.nextToken())); break;
+                case "remove" : set.remove(Integer.parseInt(st.nextToken())); break;
+                case "check" : bw.write(set.contains(Integer.parseInt(st.nextToken())) ?  1 : 0); break;
+                case "toggle" : set.contains(Integer.parseInt(st.nextToken()))? set.remove()
+            }
+        }
 
         bw.flush();
         bw.close();
     }
 
-    static void bigger(boolean[] able, int num){
-        if(able[num]){
-            if(able[num + 1]){
 
-            }
-        }
-    }
-
-    static void smaller() {
-
-    }
 
 }
 
