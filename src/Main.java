70b0f7799c3b8e26915251fa892d580a8e40ec55
Harigaze <dlrkd1122@korea.ac.kr>
2022-08-25 17:22:17 +0900

    I tried..

--- src/Main.java
+++ src/Main.java
@@ -7,19 +7,23 @@ public class Main {
 
     public static void main(String[] args) throws IOException {
         HashSet<Integer> set = new HashSet<>();
-        HashSet<Integer> allset = new HashSet<>();
+        HashSet<Integer> all = new HashSet<>();
+        for (int i = 1 ; i < 21 ;i ++)
+            all.add(i);
         StringTokenizer st;
         String input;
+        int temp;
         int n = Integer.parseInt(br.readLine());
         for (int i = 0 ; i < n ; i ++) {
             st = new StringTokenizer(br.readLine());
             input = st.nextToken();
             switch (input){
-                case "all" : set.addAll(o1 ->  if(o1 < 1))
+                case "all" : set.addAll(all); break;
+                case "empty" : set.removeAll(all); break;
                 case "add" : set.add(Integer.parseInt(st.nextToken())); break;
                 case "remove" : set.remove(Integer.parseInt(st.nextToken())); break;
-                case "check" : bw.write(set.contains(Integer.parseInt(st.nextToken())) ?  1 : 0); break;
-                case "toggle" : set.contains(Integer.parseInt(st.nextToken()))? set.remove()
+                case "check" : bw.write(set.contains(Integer.parseInt(st.nextToken()))  ?  1 +"\n" : 0 + "\n"); break;
+                case "toggle" : if(set.contains(temp = Integer.parseInt(st.nextToken()))) set.remove(temp); else set.add(temp); break;
             }
         }
 
