15f2269906c5bf209f74d72472b5e2901f5040f0
Harigaze <dlrkd1122@korea.ac.kr>
2022-08-29 17:10:37 +0900

    하는 중

--- src/Main.java
+++ src/Main.java
@@ -6,32 +6,39 @@ public class Main {
     static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
     public static void main(String[] args) throws IOException {
-        HashSet<Integer> set = new HashSet<>();
-        HashSet<Integer> all = new HashSet<>();
-        for (int i = 1 ; i < 21 ;i ++)
-            all.add(i);
-        StringTokenizer st;
-        String input;
-        int temp;
-        int n = Integer.parseInt(br.readLine());
-        for (int i = 0 ; i < n ; i ++) {
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+        int v = Integer.parseInt(st.nextToken());
+        int first , last;
+        ArrayList<node> graph = new ArrayList<>();
+        for (int i = 1 ; i <= n ; i++)
+            graph.add(new node(i, false));
+        for (int i = 0 ; i < m ; i ++){
             st = new StringTokenizer(br.readLine());
-            input = st.nextToken();
-            switch (input){
-                case "all" : set.addAll(all); break;
-                case "empty" : set.removeAll(all); break;
-                case "add" : set.add(Integer.parseInt(st.nextToken())); break;
-                case "remove" : set.remove(Integer.parseInt(st.nextToken())); break;
-                case "check" : bw.write(set.contains(Integer.parseInt(st.nextToken()))  ?  1 +"\n" : 0 + "\n"); break;
-                case "toggle" : if(set.contains(temp = Integer.parseInt(st.nextToken()))) set.remove(temp); else set.add(temp); break;
-            }
+            first = Integer.parseInt(st.nextToken()) - 1;
+            last =  Integer.parseInt(st.nextToken()) - 1;
+            graph.get(first).link.add(graph.get(last));
         }
+        dfs(graph.get(v - 1));
 
         bw.flush();
         bw.close();
     }
+    static void dfs(node root){
 
+    }
+    static void bfs(){}
 
+}
+class node {
+    PriorityQueue<node> link = new PriorityQueue<>(Comparator.comparingInt(o -> o.n));
+    public node(int n, boolean checked) {
+        this.n = n;
+        this.checked = checked;
+    }
+    int n;
+    boolean checked = false;
 
 }
 
