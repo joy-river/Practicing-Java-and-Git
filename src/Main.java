dfe766146a3ac04cc76c8efcf762d38bc40d6506
Harigaze <dlrkd1122@korea.ac.kr>
2022-09-02 17:17:19 +0900

    하는 중

--- src/Main.java
+++ src/Main.java
@@ -7,9 +7,21 @@ public class Main {
 
     public static void main(String[] args) throws IOException {
         StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+        ArrayList<String> a = new ArrayList<>();
+        LinkedHashSet<String> b = new LinkedHashSet<>();
+        String temp;
+        for (int i = 0 ; i < n;  i++)
+            b.add(br.readLine());
+        for (int i = 0 ; i < m ; i++){
+            temp = br.readLine();
+            if(temp.charAt(0) > 64)
+                bw.write(b. + 1 + "\n");
+            else
+                bw.write(a.get(Integer.parseInt(temp) - 1) + "\n");
+        }
 
-
-        bw.write(Integer.toString(b));
         bw.flush();
         bw.close();
     }
