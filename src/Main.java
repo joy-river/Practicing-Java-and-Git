7a35c5c5cdbef906e42f1a7b549f5d88171d0ddb
Harigaze <dlrkd1122@korea.ac.kr>
2022-09-14 17:13:50 +0900

    하는 중

--- src/Main.java
+++ src/Main.java
@@ -5,28 +5,26 @@ public class Main {
     static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
+
     public static void main(String[] args) throws IOException {
-        StringTokenizer st;
         int n = Integer.parseInt(br.readLine());
         int m = Integer.parseInt(br.readLine());
-        Queue<Integer>[] com = new LinkedList[n];
-        int a, b;
-        for (int i = 0 ; i < m; i++) {
-            st = new StringTokenizer(br.readLine());
+        StringTokenizer  st = new StringTokenizer(br.readLine());
 
+        for (int i = 0; i < m; i++){
+            m
         }
 
-
-        bw.write(Integer.toString());
         bw.flush();
         bw.close();
     }
 
+
+
+
 }
-class com {
-    boolean check;
-    Queue<Integer.>
-}
+
+
 
 
 
