d675ca0468dcf08ce79ff742924962b744200e2f
Harigaze <dlrkd1122@korea.ac.kr>
2022-09-16 16:14:32 +0900

    하는 중 확장 유클리드? 베주 머시기 중국인의 나머지 정리...
    잘 몰루???

--- src/Main.java
+++ src/Main.java
@@ -9,18 +9,38 @@ public class Main {
     public static void main(String[] args) throws IOException {
        int n = Integer.parseInt(br.readLine());
 
-        bw.write(Integer.toString(result));
+        EEA(10, -12);
         bw.flush();
         bw.close();
 
     }
 
 
-    static int gcd(){
+    static int gcd(int a, int b){
+        if(b == 0)
+            return a;
+
+        return gcd(b, a % b);
     }
+    static void EEA(int a, int b) throws IOException {
+        int r0=a, r1=b;
+        int s0=1, s1=0;
+        int t0=0, t1=1;
+        int temp=0,q=0;
 
-    static int xgcd(){
-
+        while(r1 != 0){
+            q=r0/r1;
+            temp=r0;
+            r0=r1;
+            r1=temp-r1*q;
+            temp=s0;
+            s0=s1;
+            s1=temp-s1*q;
+            temp=t0;
+            t0=t1;
+            t1=temp-t1*q;
+        }
+        bw.write(s0 +  "\n" + t0);
     }
 
 }
