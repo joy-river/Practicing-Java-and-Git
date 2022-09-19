0f8648e98684531ecb9246d8d06cc10333d7352c
Harigaze <dlrkd1122@korea.ac.kr>
2022-09-19 17:15:11 +0900

    하는 중 좀  더 생각 ㄱㄱ
    2 2 1 1 같은 케이스 잘 생각 ㄱ

--- src/Main.java
+++ src/Main.java
@@ -10,15 +10,38 @@ public class Main {
        int n = Integer.parseInt(br.readLine());
        StringTokenizer st;
        int[] input = new int[4];
-       int[] ts = new int[2];
+       int count,first, temp;
        for (int i = 0 ; i < n ; i ++){
            st = new StringTokenizer(br.readLine());
            for (int j = 0; j < 4; j++)
                input[j] = Integer.parseInt(st.nextToken());
-           if(input[3] - input[2] % gcd(input[0], input[1]) != 0)
+
+           first = input[2] % input[1] == 0 ? input[1] : input[2] % input[1];
+
+           if((input[3] - input[2]) % gcd(input[0] , input[1]) != 0)
                bw.write(-1 + "\n");
-           else
-               xgcd(input[0], -input[1], ts);
+
+           else if(input[2] % input[1] == 0 ? input[1] == input[3] : input[2] % input[1] == input[3])
+               bw.write(input[1] + "\n");
+
+           else{
+               count = 1;
+              while(true){
+                  temp =  (count * input[0] + input[2]) % input[1];
+                  if(temp == 0)
+                      temp = input[1];
+                  if(temp == first) {
+                      bw.write(-1 + "\n");
+                      break;
+                  }
+                  else if (temp == input[3]){
+                      bw.write(count * input[0] + input[2] + "\n");
+                      break;
+                  }
+                  else
+                      count++;
+              }
+           }
 
        }
 
