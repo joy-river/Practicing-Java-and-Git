2585994c8b23b89fe3103687765fa504e927d8e9
Harigaze <dlrkd1122@korea.ac.kr>
2022-09-19 22:46:55 +0900

    노력중
    토마토 주변 다 큐에 추가하는 메소드 만들자

--- src/Main.java
+++ src/Main.java
@@ -7,54 +7,50 @@ public class Main {
 
 
     public static void main(String[] args) throws IOException {
-       int n = Integer.parseInt(br.readLine());
-       StringTokenizer st;
-       int[] input = new int[4];
-       int count,first, temp;
-       for (int i = 0 ; i < n ; i ++){
+       StringTokenizer st = new StringTokenizer(br.readLine());
+       int m = Integer.parseInt(st.nextToken());
+       int n = Integer.parseInt(st.nextToken());
+       int h = Integer.parseInt(st.nextToken());
+
+       tomato[][] toma = new tomato[m][n * h];
+
+       Queue<tomato> a = new LinkedList<>();
+
+       for (int i = 0 ; i< n * h; i++) {
            st = new StringTokenizer(br.readLine());
-           for (int j = 0; j < 4; j++)
-               input[j] = Integer.parseInt(st.nextToken());
-
-           first = input[2] % input[1] == 0 ? input[1] : input[2] % input[1];
-
-           if((input[3] - input[2]) % gcd(input[0] , input[1]) != 0)
-               bw.write(-1 + "\n");
-
-           else if(first == input[3])
-               bw.write(input[2] + "\n");
-
-           else{
-               count = 1;
-              while(true){
-                  temp =  (count * input[0] + input[2]) % input[1];
-                  if(temp == 0)
-                      temp = input[1];
-                  if(temp == first) {
-                      bw.write(-1 + "\n");
-                      break;
-                  }
-                  else if (temp == input[3]){
-                      bw.write(count * input[0] + input[2] + "\n");
-                      break;
-                  }
-                  else
-                      count++;
-              }
-           }
-
+           for (int j = 0; j < m ; j++)
+               toma[j][i] = new tomato(Integer.parseInt(st.nextToken()), j,i,false);
        }
+
+       for (int i = 0 ; i < m; i ++){
+           for (int j = 0 ; j < n * h; j++){
+               if(toma[i][j].status == 1 && !toma[i][j].vis){
+                   add(toma[i][j]);
+               }
+           }
+       }
+
+
+   
 
         bw.flush();
         bw.close();
 
     }
+    static class tomato {
+        int status;
 
-    static int gcd(int a, int b){
-        if(b == 0)
-            return a;
+        public tomato(int status, int x, int y, boolean vis) {
+            this.status = status;
+            this.x = x;
+            this.y = y;
+            this.vis = vis;
+        }
 
-        return gcd(b, a % b);
+        int x;
+        int y;
+        boolean vis;
+
     }
 
 }
