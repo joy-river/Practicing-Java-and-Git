555703f2724b4fffdb8d6d07b1672cf0bd444185
Harigaze <dlrkd1122@korea.ac.kr>
2022-09-20 17:12:07 +0900

    하는 중
    도마도 다른건 다 괜찮은데
    5 3 2
    0 -1 0 0 0
    -1 -1 0 1 1
    0 0 0 1 1
    0 0 0 0 0
    0 0 0 0 0
    0 0 0 0 0
    
    출력 : 5
    
    정답 : 5
    
    4 3 2
    1 1 1 1
    1 1 1 1
    1 1 1 1
    1 1 1 1
    -1 -1 -1 -1
    0 1 1 -1
    
    출력 : 1
    정답 : 1
    
    5 3 1
    0 0 0 0 0
    0 1 0 1 0
    0 0 0 0 0
    
    출력 : 2
    정답 : 2
    
    4 2 1
    0 0 0 1
    0 0 0 0
    
    출력 : 4
    정답 : 4
    
    이거 함 잘 생각해보자.

--- src/Main.java
+++ src/Main.java
@@ -4,35 +4,46 @@ import java.util.*;
 public class Main {
     static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-
+    static Queue<tomato> a = new LinkedList<>();
+    static tomato[][]toma;
+    static int m, n ,h;
 
     public static void main(String[] args) throws IOException {
        StringTokenizer st = new StringTokenizer(br.readLine());
-       int m = Integer.parseInt(st.nextToken());
-       int n = Integer.parseInt(st.nextToken());
-       int h = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken()); //col
+        n = Integer.parseInt(st.nextToken()); //row
+        h = Integer.parseInt(st.nextToken());
+        int temp, count = 0;
 
-       tomato[][] toma = new tomato[m][n * h];
-
-       Queue<tomato> a = new LinkedList<>();
+       toma = new tomato[n * h][m];
 
        for (int i = 0 ; i< n * h; i++) {
            st = new StringTokenizer(br.readLine());
            for (int j = 0; j < m ; j++)
-               toma[j][i] = new tomato(Integer.parseInt(st.nextToken()), j,i,false);
+               toma[i][j] = new tomato(Integer.parseInt(st.nextToken()), i,j,false);
        }
 
-       for (int i = 0 ; i < m; i ++){
-           for (int j = 0 ; j < n * h; j++){
-               if(toma[i][j].status == 1 && !toma[i][j].vis){
-                   add(toma[i][j]);
-               }
+       for (int i = 0 ; i < n * h; i ++){
+           for (int j = 0 ; j < m; j++)
+               if(toma[i][j].status == 1 && !toma[i][j].vis)
+                   toma[i][j].add();
+       }
+       while(!a.isEmpty()){
+           count++;
+           temp = a.size();
+           for (int i = 0 ; i < temp; i++){
+               a.peek().status = 1;
+               a.peek().vis = false;
+               a.poll().add();
            }
        }
+       for (int i = 0 ; i < n * h; i++)
+           for (int j = 0; j< m; j++)
+               if(!toma[i][j].vis)
+                   count = -1;
 
 
-   
-
+        bw.write(Integer.toString(count));
         bw.flush();
         bw.close();
 
@@ -45,6 +56,32 @@ public class Main {
             this.x = x;
             this.y = y;
             this.vis = vis;
+        }
+        public void add(){
+
+            if(this.vis)
+                return;
+            else
+                this.vis = true;
+
+            if(this.status == 1) {
+                if (this.x % n > 0)
+                    toma[this.x - 1][y].add();
+                if (this.x % n < n - 1)
+                    toma[this.x + 1][y].add();
+                if (this.y  > 0)
+                    toma[this.x][this.y -1].add();
+                if (this.y  < m - 1)
+                    toma[this.x][this.y + 1].add();
+                if(this.x / n + 1 > 1 )
+                    toma[this.x - n][this.y].add();
+                if(this.x / n + 1 < h )
+                    toma[this.x + n][this.y].add();
+            }
+            else if(this.status == 0)
+                a.add(this);
+
+
         }
 
         int x;
