ed1caf881f8b14301ee6dd2353f2b5e3cc888e89
Harigaze <dlrkd1122@korea.ac.kr>
2022-09-23 11:20:28 +0900

    백준 7576 토마토 bfs문제
    다른 토마토 문제에서 높이(층 쌓기)를 뺀 버전임. 좀 더 쉽고 코드 약간 최적화했음. 날먹

---
+++ src/DFSandBFS/Toma2.java
@@ -0,0 +1,102 @@
+package DFSandBFS;
+
+import java.io.*;
+import java.util.*;
+
+public class Toma2 {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    static Queue<tomato> a = new LinkedList<>();
+    static tomato[][]toma;
+    static int m, n;
+
+    public static void main(String[] args) throws IOException {
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        m = Integer.parseInt(st.nextToken()); //col
+        n = Integer.parseInt(st.nextToken()); //row
+        int temp, count = 0;
+        boolean zero = false;
+
+        toma = new tomato[n][m];
+
+        for (int i = 0 ; i< n; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < m ; j++) {
+                toma[i][j] = new tomato(Integer.parseInt(st.nextToken()), i, j, false);
+                if(toma[i][j].status == 0)
+                    zero = true;
+            }
+        }
+        if(zero) {
+            for (int i = 0; i < n; i++) {
+                for (int j = 0; j < m; j++)
+                    if (toma[i][j].status == 1 && !toma[i][j].vis)
+                        toma[i][j].add();
+            }
+            while (!a.isEmpty()) {
+                count++;
+                temp = a.size();
+                for (int i = 0; i < temp; i++) {
+                    a.peek().status = 1;
+                    a.peek().vis = false;
+                    a.poll().add();
+                }
+            }
+            for (int i = 0; i < n; i++)
+                for (int j = 0; j < m; j++)
+                    if (toma[i][j].status == 0) {
+                        count = -1;
+                        break;
+                    }
+        }
+
+        bw.write(Integer.toString(count));
+        bw.flush();
+        bw.close();
+
+    }
+    static class tomato {
+        int status;
+
+        public tomato(int status, int x, int y, boolean vis) {
+            this.status = status;
+            this.x = x;
+            this.y = y;
+            this.vis = vis;
+        }
+        public void add(){
+
+            if(this.vis)
+                return;
+            else
+                this.vis = true;
+
+            if(this.status == 1) {
+                if (this.x % n > 0)
+                    toma[this.x - 1][y].add();
+                if (this.x % n < n - 1)
+                    toma[this.x + 1][y].add();
+                if (this.y  > 0)
+                    toma[this.x][this.y -1].add();
+                if (this.y  < m - 1)
+                    toma[this.x][this.y + 1].add();
+            }
+            else if(this.status == 0)
+                a.add(this);
+        }
+
+        int x;
+        int y;
+        boolean vis;
+
+    }
+
+}
+
+
+
+
+
+
+
+
