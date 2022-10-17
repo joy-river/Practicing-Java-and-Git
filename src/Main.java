3c05ed34879b3fe7fb02a40003c633f25bd92e2b
Harigaze <dlrkd1122@korea.ac.kr>
2022-10-17 17:13:46 +0900

    하는 중
    12
    1 2 1 3 1 -1
    2 1 1 4 3 5 5 -1
    3 1 1 6 1 -1
    4 7 2 2 3 -1
    5 2 5 8 4 9 2 -1
    6 3 1 10 1 11 1 -1
    7 4 2 -1
    8 5 4 12 5 -1
    9 5 2 -1
    10 6 1 -1
    11 6 1 -1
    12 8 5 -1

--- src/Main.java
+++ src/Main.java
@@ -4,61 +4,73 @@ import java.util.*;
 public class Main {
     static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-    static int result  = 0;
-    static int[][] wire;
-    public static void main(String[] args) throws IOException {
-            int n = Integer.parseInt(br.readLine());
-            wire = new int[n + 1][n + 1];
-            node1[] tree = new node1[n + 1];
-            int a, b;
-            for (int i = 1; i <= n; i++)
-                tree[i] = new node1(i, new ArrayList<>(), false);
 
-            for (int i = 1 ; i <= n; i++) {
-                StringTokenizer st = new StringTokenizer(br.readLine());
-                st.nextToken();
-                while(true) {
-                    a = Integer.parseInt(st.nextToken());
-                    if (a == -1)
-                        break;
-                    else {
-                        b = Integer.parseInt(st.nextToken());
-                        wire[i][a] = b;
-                        wire[a][i] = b;
-                        tree[i].link.add(tree[a]);
-                        tree[a].link.add(tree[i]);
-                    }
+    public static void main(String[] args) throws IOException {
+        int n = Integer.parseInt(br.readLine());
+        node1[] tree = new node1[n + 1];
+        int temp, a, b, result = 0;
+        for (int i = 1; i <= n; i++)
+            tree[i] = new node1(i, new ArrayList<>(), new ArrayList<>(), false);
+
+        for (int i = 1; i <= n; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            temp = Integer.parseInt(st.nextToken());
+            while (true) {
+                a = Integer.parseInt(st.nextToken());
+                if (a == -1)
+                    break;
+                else {
+                    b = Integer.parseInt(st.nextToken());
+                    tree[temp].weight.add(b);
+                    tree[a].weight.add(b);
+                    tree[temp].link.add(tree[a]);
+                    tree[a].link.add(tree[temp]);
                 }
             }
-            for  (int i = 1; i <= n; i++)
-                result = Math.max(result, dfs(tree[i]));
+        }
+        for (int i = 1; i <= n; i++)
+            if (!tree[i].visited)
+                result = Math.max(result, init(tree[i]));
 
 
-            bw.write(Integer.toString(result));
-            bw.flush();
-            bw.close();
+        bw.write(Integer.toString(result));
+        bw.flush();
+        bw.close();
 
+    }
+
+    static int init (node1 root){
+        int temp = 0;
+        root.visited = true;
+        for (int i = 0; i < root.link.size(); i++){
+            if(!root.link.get(i).visited){
+                temp += root.weight.get(i) + dfs(root.link.get(i));
+            }
+        }
+        return temp;
     }
     static int dfs (node1 root){
         int temp = 0;
         if(!root.visited){
             root.visited = true;
-            for (int i = 0  ; i <  root.link.size(); i++)
-                if(root.link.get(i).num > root.num)
-                    temp = Math.max(temp, wire[root.num][root.link.get(i).num] + dfs(root.link.get(i)));
+            for (int i = 0  ; i < root.link.size(); i++)
+                if(!root.link.get(i).visited)
+                    temp = Math.max(temp, root.weight.get(i) + dfs(root.link.get(i)));
         }
         return temp;
     }
 }
 class node1 {
-    public node1(int num, ArrayList<node1> link, boolean visited) {
+    public node1(int num, ArrayList<node1> link, ArrayList<Integer> weight, boolean visited) {
         this.num = num;
         this.link = link;
+        this.weight = weight;
         this.visited = visited;
     }
 
     int num;
     ArrayList<node1> link;
+    ArrayList<Integer> weight;
     boolean visited;
 
 }
