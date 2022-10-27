a37fbe86745d5f5778344bdcc4a192d23d8671ee
Harigaze <dlrkd1122@korea.ac.kr>
2022-10-27 12:59:44 +0900

    백준 1865번 웜홀 벨만 포드 알고리즘
    자. 이 문제 야발이 하고싶은 말은 실제로 원래 자리로 돌아오고, 그게 시간이 얼마나 줄어드는지가 궁금한 것이냐? 절대 아니다. 즉 실제 값을 구할 수는 있지만, 구할 필요가 없고, 구할 경우 시간초과가 나게끔 되어있다. 그럼 문제가 물어보는 것이 뭐냐면 그래프 전체에 음의 사이클이 있냐 없냐이다. 사이클을 통해서 감소된 시간으로 원래 자리로 돌아올 수 있느냐를 묻고 싶은것. 즉 우리가 할것은 전체 그래프에 음의 사이클이 있냐 없냐만 찾아내면 된다.
    
    그러므로 bell(=dist)거리 배열을 전부 0으로 해놓고 벨만 포드를 돌렸을때. 음의 사이클이 있는지를 바로 찾을 수 있으므로 문제를 풀 수 있다. 만약 실제 거리를 묻는다면 각 정점에 대해 벨만 포드를 돌려야된다.

---
+++ src/GraphTheory/WormHole.java
@@ -0,0 +1,86 @@
+package GraphTheory;
+
+import java.io.*;
+import java.util.*;
+
+public class WormHole {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    static final int inf = Integer.MAX_VALUE;
+
+    public static void main(String[] args) throws IOException {
+        int tc = Integer.parseInt(br.readLine());
+
+        for (int i = 0 ; i < tc; i ++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int n = Integer.parseInt(st.nextToken());
+            int m = Integer.parseInt(st.nextToken());
+            int w = Integer.parseInt(st.nextToken());
+            boolean yn = false;
+            HashSet<Edge> edge = new HashSet<>();
+            HashSet<Integer> start = new HashSet<>();
+
+            int [] bell = new int[n + 1];
+            int [][] weight = new int[n + 1][n + 1];
+            for (int j = 1; j <= n ; j++)
+                Arrays.fill(weight[j], inf);
+
+            for (int j = 0 ; j < m + w; j++){
+                st = new StringTokenizer(br.readLine());
+                int u = Integer.parseInt(st.nextToken());
+                int v = Integer.parseInt(st.nextToken());
+                int x = Integer.parseInt(st.nextToken());
+
+                start.add(u);
+                edge.add(new Edge(u, v));
+
+                if(j < m) {
+                    edge.add(new Edge(v, u));
+                    start.add(v);
+                    weight[u][v] = Math.min(weight[u][v], x);
+                    weight[v][u] = Math.min(weight[v][u], x);
+                }
+
+                else
+                    weight[u][v] = Math.min(weight[u][v], -1 * x);
+            }
+
+
+            for (int s = 0; s <= n ; s++){
+                for (Edge value : edge) {
+                    int from = value.from;
+                    int to = value.to;
+                    int wei = weight[from][to];
+                    if (bell[from] != inf)
+                        if (bell[to] > bell[from] + wei)
+                            if(s == n) {
+                                yn=  true;
+                                break;
+                            }
+                            else
+                                bell[to] = bell[from] + wei;
+                }
+            }
+            bw.write(yn ? "YES\n" : "NO\n");
+        }
+
+
+        bw.flush();
+        bw.close();
+    }
+
+}
+
+class Edge{
+    int from;
+    int to;
+    public Edge(int from, int to) {
+        this.from = from;
+        this.to = to;
+    }
+}
+
+
+
+
+
