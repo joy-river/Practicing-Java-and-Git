32cb47f4675dff68345181b4fe41eb40e88e9ebf
Harigaze <dlrkd1122@korea.ac.kr>
2022-11-01 17:15:30 +0900

    메모리 초과 오지게 뜸.

--- src/Main.java
+++ src/Main.java
@@ -4,126 +4,90 @@ import java.util.*;
 public class Main {
     static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-    static int count;
+
     public static void main(String[] args) throws IOException {
-
-        int chan = Integer.parseInt(br.readLine());
-        int beeg = chan != 0 ? (int) Math.log10(chan) : 0;
         int n = Integer.parseInt(br.readLine());
-        StringTokenizer st = null;
-        if(n != 0) st = new StringTokenizer(br.readLine());
-        boolean[] able = new boolean[10];
+        for (int i = 0; i < n ; i ++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            Queue<dslr> bs = new LinkedList<>();
+            bs.add(new dslr("", a));
+            while (!bs.isEmpty()) {
+                int num = bs.peek().num;
+                if (num == b) {
+                    bw.write(bs.peek().sb.toString() + "\n");
+                    break;
+                }
+                if (num != 0) {
+                    bs.add(new dslr(bs.peek().sb +"D", d(num)));
+                    bs.add(new dslr(bs.peek().sb +"L", l(Integer.toString(num))));
+                    bs.add(new dslr(bs.peek().sb +"R", r(Integer.toString(num))));
+                }
+                bs.add(new dslr(bs.peek().sb +"S", s(num)));
 
-        count = Math.abs(chan - 100);
+                bs.poll();
 
-        for (int i = 0 ; i < n ; i ++)
-            able[Integer.parseInt(st.nextToken())] = true;
-
-        if(count <= beeg || n == 10)
-            bw.write(Integer.toString(count));
-        else{
-            int big = bigger(able, chan, beeg);
-            int smol = smaller(able,chan,beeg);
-
-            smol = Math.min(Math.abs(big - chan) + (big != 0 ? (int)Math.log10(big) + 1 : 1) , Math.abs(smol - chan) + (smol != 0 ? (int)Math.log10(smol) + 1 : 1));
-
-            if(!able[chan / (int) Math.pow(10, beeg)]){
-                int sam = same(able, chan, beeg);
-                smol = Math.min(smol , Math.abs(sam - chan) + (sam != 0 ? (int)Math.log10(sam) + 1 : 1));
-
-                bw.write(Integer.toString(Math.min(smol, count)));
-            }
-            else{
-                bw.write(Integer.toString(Math.min(smol, count)));
             }
         }
+
         bw.flush();
         bw.close();
     }
 
-    static int bigger(boolean[] able, int num, int digit){
-        int temp = 9999999;
-        int first = num / (int) Math.pow(10,digit);
-       for (int i = first; i < 10; i ++){
-           if(!able[i]){
-               temp = i * (int)Math.pow(10,digit--);
-               for (int j = 0 ; j < 10 ; j++){
-                   if(!able[j]) {
-                       for (int s = 0; s <= digit; s++)
-                           temp += j * (int) Math.pow(10, s);
-                       break;
-                   }
-               }
-               return temp;
-           }
-       }
-       for (int i = 1; i < first; i ++){
-           if(!able[i]){
-               temp = i * (int)Math.pow(10, digit + 1);
-               for (int j = 0 ; j < 10 ; j++){
-                   if(!able[j]) {
-                       for (int s = 0; s <= digit; s++)
-                           temp += j * (int) Math.pow(10, s);
-                       break;
-                   }
-               }
-               return temp;
-           }
 
-       }
-        return temp;
+    static int d(int num){
+        return (2 * num) % 10000;
     }
-    static int same(boolean[]able, int num,int digit) {
-        if(digit < 0)
-            return 0;
-
-        int a = (int) (num / Math.pow(10, digit));
-        int result = 100;
-        for (int i = 1; i < 10; i  ++)
-            if(!able[i])
-                if(Math.abs(result - a) > Math.abs(i - a))
-                    result = i;
-
-        return (int) (result * Math.pow(10, digit) + same(able, (int) (num % Math.pow(10, digit)), digit - 1));
+    static int s(int num){
+        return num == 0 ? 9999 : num - 1;
     }
+    static int l(String num){
+        String[] split = num.split("");
+        StringBuilder sb = new StringBuilder();
 
-    static int smaller(boolean[] able, int num, int digit) {
-        int temp = 9999999;
-        if (num != 0) {
-            int first = num / (int) Math.pow(10, digit) - 1;
-            for (int i = first; i >= 0; i--) {
-                if (!able[i]) {
-                    temp = i * (int) Math.pow(10, digit--);
-                    for (int j = 9; j >= 0; j--) {
-                        if (!able[j]) {
-                            for (int s = 0; s <= digit; s++)
-                                temp += j * (int) Math.pow(10, s);
-                            break;
-                        }
-                    }
-                    return temp;
-                }
-            }
-            if(digit > 0) {
-                for (int i = 9; i > first; i--) {
-                    if (!able[i]) {
-                        temp = i * (int) Math.pow(10, --digit);
-                        for (int j = 9; j >= 0; j--) {
-                            if (!able[j]) {
-                                for (int s = 0; s <= digit - 1; s++)
-                                    temp += j * (int) Math.pow(10, s);
-                                break;
-                            }
-                        }
-                        return temp;
-                    }
-                }
-            }
+        if(split.length == 4){
+            for (int i = 1;i < split.length; i++)
+                sb.append(split[i]);
+            sb.append(split[0]);
         }
-        return temp;
+        else {
+            for (int i = 0; i < split.length; i++)
+                sb.append(split[i]);
+            sb.append("0");
+        }
+
+        return Integer.parseInt(sb.toString());
+    }
+    static int r(String num){
+        String[] split = num.split("");
+        StringBuilder sb = new StringBuilder();
+        int n = 4 - split.length;
+
+        if(split.length == 4) {
+            sb.append(split[split.length - 1]);
+            for (int i = 0; i < split.length - 1; i++)
+                sb.append(split[i]);
+        }
+        else{
+            sb.append(split[split.length - 1]);
+            for (int i = 0 ; i < n ; i++)
+                sb.append("0");
+            for (int i = 0 ; i < split.length - 1; i ++)
+                sb.append(split[i]);
+        }
+
+        return Integer.parseInt(sb.toString());
+    }
+}
+class dslr{
+    public dslr(String sb, int num) {
+        this.sb = sb;
+        this.num = num;
     }
 
+    String sb;
+    int num;
 }
-
 
 
