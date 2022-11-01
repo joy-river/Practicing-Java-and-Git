0f14362814d0f0cd4961f02ad683f91270ac1a27
Harigaze <dlrkd1122@korea.ac.kr>
2022-11-01 12:17:51 +0900

    백준 1107번 리모컨 브루트 포스
    이렇게 쉬운걸 이 지랄을 해서 풀었다니 믿기지가 않는다. 좀 더 쉽게 풀려다가 한참 돌아갔었음. 지금 알고리즘은 가고싶은 채널에서 + - next해가면서 그 값이 valid 한 값인지 검사하는 거임. 모든 자리 수가 다 입력가능한지 판단하는 간단한 알고리즘이지만 시간은 꽤 오래 걸릴것.. 좀 더 쉽게 할 방법이 없을까 해서 작을때 클때  구분해서 접근하는 방식으로 했었는데 결국 실패.

--- src/BruteForce/RemoteControi.java
+++ src/BruteForce/RemoteControi.java
@@ -10,32 +10,48 @@ public class RemoteControi {
     public static void main(String[] args) throws IOException {
 
         int chan = Integer.parseInt(br.readLine());
-        int beeg = (int) Math.log10(chan);
+        int beeg = chan != 0 ? (int) Math.log10(chan) : 0;
         int n = Integer.parseInt(br.readLine());
-        StringTokenizer st = new StringTokenizer(br.readLine());
+        StringTokenizer st = null;
+        if(n != 0) st = new StringTokenizer(br.readLine());
         boolean[] able = new boolean[10];
 
-        count = chan > 100 ? chan - 100 : 100 - chan;
+        int next = 0;
+
+        count = Math.abs(chan - 100);
+
         for (int i = 0 ; i < n ; i ++)
             able[Integer.parseInt(st.nextToken())] = true;
+        if(n != 10) {
+            while (true) {
+                if (chan - next >= 0)
+                    if (valid(Integer.toString(chan - next), able)) {
+                        chan -= next;
+                        break;
+                    }
 
+                if (valid(Integer.toString(chan + next), able)) {
+                    chan += next;
+                    break;
+                }
+                next++;
+            }
 
+            bw.write(Integer.toString(Math.min(count, (int) (next + (chan == 0 ? 1 : (Math.log10(chan) + 1))))));
+        }
+        else
+            bw.write(Integer.toString(count));
         bw.flush();
         bw.close();
     }
 
-    static void bigger(boolean[] able, int num){
-        if(able[num]){
-            if(able[num + 1]){
-
-            }
-        }
+    static boolean valid(String chan, boolean[] able){
+        String[] digit = chan.split("");
+        for (int i = 0; i < digit.length; i++)
+            if(able[Integer.parseInt(digit[i])])
+                return false;
+        return true;
     }
-
-    static void smaller() {
-
-    }
-
 }
 
 
