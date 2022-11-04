13047832f31cd4648cceaae2283502efc5cc5689
Harigaze <dlrkd1122@korea.ac.kr>
2022-11-04 13:14:15 +0900

    백준 11403번 경로 찾기 플로이드 워셜 문제
    그냥 플로이드 워셜로 현재 0인 경로에다가 깔쌈하게 적용해주면 됨.

--- src/GraphTheory/FindPath.java
+++ src/GraphTheory/FindPath.java
@@ -1,37 +1,35 @@
-import java.io.*;
-import java.util.*;
+package GraphTheory;
 
-public class Main {
+import java.io.*;
+
+public class FindPath {
 	    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 	        static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
 
 		    public static void main(String[] args) throws IOException {
-			            int n = Integer.parseInt(br.readLine());
-				            String[][] table = new String[n][];
+				int n = Integer.parseInt(br.readLine());
+				String[][] table = new String[n][];
 
-					            for (int i = 0 ; i < n ; i ++)
-							                table[i] = br.readLine().split(" ");
+				for (int i = 0 ; i < n ; i ++)
+					table[i] = br.readLine().split(" ");
 
-						            for (int i = 0 ; i < n ; i ++)
-								                for (int j = 0 ; j < n ; j ++)
-											                for (int s = 0 ; s < n; s++)
-														                    if(table[j][s].equals("0"))
-																	                         table[j][s] = table[j][i].equals("1") && table[i][s].equals("1") ? "1" : "0";
-
-
-
-
-							            for (int i = 0; i < n; i ++) {
-									                for (int j = 0; j < n; j++)
-												                bw.write(table[i][j] + " ");
-											            bw.write("\n");
-												            }
-								            bw.flush();
-									            bw.close();
-										        }
+				for (int i = 0 ; i < n ; i ++)
+					for (int j = 0 ; j < n ; j ++)
+						for (int s = 0 ; s < n; s++)
+							if(table[j][s].equals("0"))
+								table[j][s] = table[j][i].equals("1") && table[i][s].equals("1") ? "1" : "0";
 
 
 
+
+				for (int i = 0; i < n; i ++) {
+					for (int j = 0; j < n; j++)
+						bw.write(table[i][j] + " ");
+					bw.write("\n");
+				}
+				bw.flush();
+				bw.close();
+			}
 
 }
