f81c079154680bbe2931893d7a3a7f65af5cc251
Harigaze <dlrkd1122@korea.ac.kr>
2022-11-08 17:07:42 +0900

    하는중

--- src/Main.java
+++ src/Main.java
@@ -6,15 +6,38 @@ public class Main {
     static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
     public static void main(String[] args) throws IOException {
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        int n = Integer.parseInt(st.nextToken());
-        int m = Integer.parseInt(st.nextToken());
+        String[] input = br.readLine().split("");
+        StringBuilder sb = new StringBuilder();
+        Stack<String> oper = new Stack<>();
+        Stack<String> result = new Stack<>();
+
+        boolean b =false;
+        for (String s : input) {
+            if (s.charAt(0) - 65 >= 0)
+                result.add(s);
+            else if (s.equals("("))
+                b = true;
+            else if (s.equals(")")) {
+                sb.append(print(alpha, bracket));
+                b = false;
+            }
+            else if (s.equals("+") || s.equals("-")) {
+                if (!bracket.isEmpty())
+                    if (bracket.peek().equals("*") || bracket.peek().equals("/"))
+                        if(!b)
+                            sb.append(print(alpha, bracket));
+                bracket.push(s);
+            }
+            else
+                bracket.push(s);
+        }
+        sb.append(print(alpha,bracket));
 
 
+        bw.write(sb.toString());
         bw.flush();
         bw.close();
     }
-
 
 }
 
