729f04fe392cba35be8a0f33ef0714833f044197
Harigaze <dlrkd1122@korea.ac.kr>
2022-11-29 16:21:05 +0900

    백준 1916번 최소비용구하기 다익스트라 문제
    작성자님께서 ﻿﻿﻿﻿33,34번 줄에서 저장된 값이 0일 경우 입력 받은 수를 그대로 넣어주는 방식으로 코드를 짜셨는데, 이 문제에서는 버스의 비용이 0인 경우가 존재합니다. 따라서 배열에 값이 들어온 적이 없는것인지, 혹은 배열에 값이 들어왔는데 0인지를 판단할 수 없습니다. 여기서 문제가 발생한 것 같습니다.
    예를 들자면
    
    1 4 3
    
    1 4 0
    
    1 4 1
    
    같은 입력이 들어올 경우 원래라면 최소 비용인 0이 배열에 저장되어야하지만, 현재 코드 상으로 0이 저장된 이후에 배열에 바로 다음 값인 1이 저장되게 되므로 최소 비용을 저장하지 못합니다. 이 부분을 고치신다면 통과할 수 있으실것 같습니다.
    단순한 다익스트라였는데 비용을 입력받을 때 삐꾸나서 틀림.

---
+++ src/DFSandBFS/MinimumCost.java
@@ -0,0 +1,81 @@
+package DFSandBFS;
+
+import java.io.*;
+import java.util.*;
+
+public class MinimumCost {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+    public static void main(String[] args) throws IOException {
+        int n = Integer.parseInt(br.readLine());
+        int m = Integer.parseInt(br.readLine());
+        StringTokenizer st;
+        int a, b, temp;
+        Bus poll;
+        Bus[] list = new Bus[n + 1];
+        int [] dij = new int[n + 1];
+        PriorityQueue<Bus> next = new PriorityQueue<>(Comparator.comparingInt(o -> dij[o.num]));
+        int [][] cost = new int [n + 1][n + 1];
+        Arrays.fill(dij, Integer.MAX_VALUE);
+
+        for (int i = 1; i <= n; i++) {
+            list[i] = new Bus(i, false, new ArrayList<>());
+            Arrays.fill(cost[i], Integer.MAX_VALUE);
+            cost[i][i] = 0;
+        }
+
+
+        for (int i = 0 ; i < m; i ++){
+            st = new StringTokenizer(br.readLine());
+            a = Integer.parseInt(st.nextToken());
+            b = Integer.parseInt(st.nextToken());
+            temp = Integer.parseInt(st.nextToken());
+            cost[a][b] = Math.min(cost[a][b], temp);
+            list[a].link.add(list[b]);
+        }
+
+        st = new StringTokenizer(br.readLine());
+        a = Integer.parseInt(st.nextToken());
+        b = Integer.parseInt(st.nextToken());
+
+        dij[a] = 0;
+        next.add(list[a]);
+
+        while(!next.isEmpty()){
+            poll = next.poll();
+            if(!poll.visited) {
+                poll.visited = true;
+                for (int i = 0; i < poll.link.size(); i++) {
+                    dij[poll.link.get(i).num] = Math.min(dij[poll.link.get(i).num], dij[poll.num] + cost[poll.num][poll.link.get(i).num]);
+                    next.add(poll.link.get(i));
+                }
+
+            }
+        }
+
+        bw.write(Integer.toString(dij[b]));
+        bw.flush();
+        bw.close();
+    }
+
+}
+class Bus {
+    int num;
+    boolean visited;
+    ArrayList<Bus> link;
+
+    public Bus(int num, boolean visited, ArrayList<Bus> link) {
+        this.num = num;
+        this.visited = visited;
+        this.link = link;
+    }
+}
+
+
+
+
+
+
+
+
