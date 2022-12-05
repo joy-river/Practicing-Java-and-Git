fbbfa165705d31a6c0e289dc8abf3b389bd74227
Harigaze <dlrkd1122@korea.ac.kr>
2022-12-05 17:14:01 +0900

    거의 다 됨.

--- src/Main.java
+++ src/Main.java
@@ -4,119 +4,43 @@ import java.util.*;
 public class Main {
     static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-    static int n , m;
-    static String[][] input;
-
     public static void main(String[] args) throws IOException {
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        n = Integer.parseInt(st.nextToken());
-        m = Integer.parseInt(st.nextToken());
+        ArrayList<Integer> list = new ArrayList<>();
+        String a;
+        while (!(a = br.readLine()).equals(""))
+            list.add(Integer.parseInt(a));
 
-        input = new String[n][];
-        for (int i = 0 ; i < n ; i++)
-            input[i] = br.readLine().split(" ");
+        Tree(list, 0, list.size() - 1);
 
-        bw.write(Integer.toString(melt()));
         bw.flush();
         bw.close();
     }
+    static void Tree(ArrayList<Integer> list, int start, int end) throws IOException {
+        int root = list.get(start);
+        int i = start + 1;
 
-    static int melt(){
-        HashSet<Index> entry = new HashSet<>();
-        int[][] visited = new int[n][m];
-        int x = 0, y = 0, time = 0;
-
-        while(true){
-            bfs(-1 * time, entry, visited);
-            if(entry.isEmpty())
-                break;
-
-            for(Index a : entry){
-                x = a.x;
-                y = a.y;
-                if(visited[x][y] > 1) {
-                    input[x][y] = "0";
-                    visited[x][y] = -1 * time - 1;
+        if(start >= end)
+            bw.write(root + "\n");
+        else {
+            while (i <= end) {
+                if (root < list.get(i)) {
+                    if(i > start + 1)
+                        Tree(list, start + 1, i - 1);
+                    Tree(list, i, end);
+                    break;
                 }
                 else
-                    visited[x][y] = 0;
+                    i++;
             }
-            entry.clear();
-            time++;
+            if(i == end + 1)
+                Tree(list, start + 1, i - 1);
+            bw.write(root + "\n");
         }
 
-        return time;
-    }
-    static void bfs(int time, HashSet<Index> entry, int[][] visited) {
-        Queue<Index> next = new LinkedList<>();
-        next.add(new Index(0, 0));
-        visited[0][0] = time;
-        int x, y;
-        Index temp;
-        while (!next.isEmpty()) {
-            temp = next.poll();
-            x = temp.x;
-            y = temp.y;
-
-            if (x > 0)
-                if (input[x - 1][y].equals("0")) {
-                    if (visited[x - 1][y] >= time) {
-                        visited[x - 1][y] = time - 1;
-                        next.add(new Index(x - 1, y));
-                    }
-                }
-                else {
-                    visited[x - 1][y]++;
-                    entry.add(new Index(x - 1, y));
-                }
-
-
-            if (x < n - 1)
-                if (input[x + 1][y].equals("0")) {
-                    if (visited[x + 1][y] >= time) {
-                        visited[x + 1][y] = time - 1;
-                        next.add(new Index(x + 1, y));
-                    }
-                }
-                else {
-                    visited[x + 1][y]++;
-                    entry.add(new Index(x + 1, y));
-                }
-
-            if (y > 0)
-                if (input[x][y - 1].equals("0")) {
-                    if (visited[x][y - 1] >= time) {
-                        visited[x][y - 1] = time - 1;
-                        next.add(new Index(x, y - 1));
-                    }
-                } else {
-                    visited[x][y - 1]++;
-                    entry.add(new Index(x, y - 1));
-                }
-
-            if (y < m - 1)
-                if (input[x][y + 1].equals("0")) {
-                    if (visited[x][y + 1] >= time) {
-                        visited[x][y + 1] = time - 1;
-                        next.add(new Index(x, y + 1));
-                    }
-                } else {
-                    visited[x][y + 1]++;
-                    entry.add(new Index(x, y + 1));
-                }
-        }
-    }
-
-}
-class Index {
-    int x;
-    int y;
-
-    public Index(int x, int y) {
-        this.x = x;
-        this.y = y;
     }
 }
+
+
 
 
 
