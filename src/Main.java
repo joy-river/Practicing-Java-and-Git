3bd88294b57bd2ac37c2026b943d6cbc1355dcd2
Harigaze <dlrkd1122@korea.ac.kr>
2022-12-06 21:08:56 +0900

    하는중

--- src/Main.java
+++ src/Main.java
@@ -1,43 +1,22 @@
 import java.io.*;
 import java.util.*;
-
 public class Main {
     static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
     public static void main(String[] args) throws IOException {
-        ArrayList<Integer> list = new ArrayList<>();
-        String a;
-        while (!(a = br.readLine()).equals(""))
-            list.add(Integer.parseInt(a));
+        String input = br.readLine();
+        String pat = br.readLine();
 
-        Tree(list, 0, list.size() - 1);
 
+        while(input.contains(pat))
+        input = input.replaceAll(pat, "");
+
+
+        bw.write(input.equals("") ? "FRULA" : input);
         bw.flush();
         bw.close();
     }
-    static void Tree(ArrayList<Integer> list, int start, int end) throws IOException {
-        int root = list.get(start);
-        int i = start + 1;
 
-        if(start >= end)
-            bw.write(root + "\n");
-        else {
-            while (i <= end) {
-                if (root < list.get(i)) {
-                    if(i > start + 1)
-                        Tree(list, start + 1, i - 1);
-                    Tree(list, i, end);
-                    break;
-                }
-                else
-                    i++;
-            }
-            if(i == end + 1)
-                Tree(list, start + 1, i - 1);
-            bw.write(root + "\n");
-        }
-
-    }
 }
 
 
