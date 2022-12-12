224f45c705a66faffe23f240c38d5040f09ebb24
Harigaze <dlrkd1122@korea.ac.kr>
2022-12-12 17:15:07 +0900

    백준 2638번 치즈 bfs문제
    외부 공기 => bfs로 1을 무시하고 0끼리 진행했을때 닿은 곳. 따라서 bfs 시작지점을 가장자리 변으로 해야(치즈가 없는 곳) 항상 외부공기라는 것을 보장할 수 있겠음. 문제가 안생김 <- 여기서 처음 틀렸음.
    
    그 후에는 제거해야할 치즈들을 골라내고 다른 것은 모두 처음(과 비슷한)상태로 되돌려서 bfs를 반복하는 것이 정답 풀이가 되겠음,.

--- src/Main.java
+++ src/Main.java
@@ -6,67 +6,12 @@ public class Main {
     static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
     public static void main(String[] args) throws IOException {
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        int n = Integer.parseInt(st.nextToken());
-        int k = Integer.parseInt(st.nextToken());
-        int pos, time, mintime = Integer.MAX_VALUE, count = 0;
-        boolean [] visited = new boolean[200001];
-        PriorityQueue<Pos> next = new PriorityQueue<>((o1, o2) -> {
-            if(o1.time == o2.time)
-                return Integer.compare(o1.Pos, o2.Pos);
-            else
-                return Integer.compare(o1.time, o2.time);
-        });
 
-        visited[n] = true;
-        next.add(new Pos(n, 1));
 
-        while(!next.isEmpty()){
-            pos = next.peek().Pos;
-            time = next.poll().time;
-
-            if(time > mintime){
-                bw.write(mintime +"\n" +count);
-                break;
-            }
-            if(pos == k){
-                count++;
-                mintime = time;
-                continue;
-            }
-
-            if(pos < k)
-                if(!visited[pos * 2]){
-                    visited[pos * 2] = true;
-                    next.add(new Pos(pos * 2, time + 1));
-                }
-            if(pos > 0)
-                if(!visited[pos -1]){
-                    visited[pos - 1] = true;
-                    next.add(new Pos(pos - 1, time + 1));
-                }
-            if(pos < 100000)
-                if(!visited[pos + 1]){
-                    visited[pos + 1] = true;
-                    next.add(new Pos(pos + 1, time + 1));
-                }
-
-        }
 
         bw.flush();
         bw.close();
     }
-
-}
-class Pos{
-    int Pos;
-    int time;
-
-    public Pos(int pos, int time) {
-        this.Pos = pos;
-        this.time = time;
-    }
-
 
 }
 
