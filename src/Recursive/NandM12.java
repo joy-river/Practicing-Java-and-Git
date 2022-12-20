036b54afd1883e1c70919c522ae2fe75be761ac2
Harigaze <dlrkd1122@korea.ac.kr>
2022-12-20 09:10:02 +0900

    백준 15666번 N과 M(12) 재귀로 푼 문제
    N과 M(8)번 소스에서 중복된 수열 출력하지 않게끔 Hashset으로 조정한게 다임.

---
+++ src/Recursive/NandM12.java
@@ -0,0 +1,50 @@
+package Recursive;
+
+import java.io.*;
+import java.util.*;
+
+public class NandM12 {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    static int n, m;
+    static HashSet<String> dupe = new HashSet<>();
+
+    public static void main(String[] args) throws IOException {
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+        ArrayList<Integer> list = new ArrayList<>();
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0 ; i < n; i++)
+            list.add(Integer.parseInt(st.nextToken()));
+
+        Collections.sort(list);
+
+        print(new StringBuilder(), list, 0, 0);
+
+        bw.flush();
+        bw.close();
+    }
+
+    static void print(StringBuilder sb, ArrayList<Integer> list, int index, int count) throws IOException {
+        if(count == m - 1)
+            while (index < n) {
+                if(!dupe.contains(sb.toString() + list.get(index))) {
+                    dupe.add(sb.toString() + list.get(index));
+                    bw.write(sb.toString() + list.get(index) + "\n");
+                }
+                index++;
+            }
+        else while(index < n)
+            print(new StringBuilder(sb).append(list.get(index)).append(" "), list, index++, count + 1);
+    }
+
+}
+
+
+
+
+
+
+
+
