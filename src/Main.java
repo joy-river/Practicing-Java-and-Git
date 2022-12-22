c406d10f1a0291bf4ad5b85ff12178d1bc543d38
Harigaze <dlrkd1122@korea.ac.kr>
2022-12-22 17:12:23 +0900

    하는중

--- src/Main.java
+++ src/Main.java
@@ -4,41 +4,61 @@ import java.util.*;
 public class Main {
     static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-    static int n, m;
-    static HashSet<String> dupe = new HashSet<>();
+
 
     public static void main(String[] args) throws IOException {
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        n = Integer.parseInt(st.nextToken());
-        m = Integer.parseInt(st.nextToken());
-        ArrayList<Integer> list = new ArrayList<>();
-        st = new StringTokenizer(br.readLine());
-        for (int i = 0 ; i < n; i++)
-            list.add(Integer.parseInt(st.nextToken()));
+        int n = Integer.parseInt(br.readLine());
+        String[][] table = new String [n][];
+        for (int i = 0 ; i < n ; i++)
+            table[i] = br.readLine().split(" ");
 
-        Collections.sort(list);
-
-        print(new StringBuilder(), list, 0, 0);
+        bw.write(Integer.toString(bfs(table, n)));
 
         bw.flush();
         bw.close();
     }
+    //가로 : 0, 세로 : 1, 대각 : 2
+    static int bfs(String[][] table, int n){
+        int count = 0, x, y;
+        Pipe temp;
+        Queue<Pipe> next = new LinkedList<>();
+        next.add(new Pipe(0, 1 , 0 ));
 
-    static void print(StringBuilder sb, ArrayList<Integer> list, int index, int count) throws IOException {
-        if(count == m - 1)
-            while (index < n) {
-                if(!dupe.contains(sb.toString() + list.get(index))) {
-                    dupe.add(sb.toString() + list.get(index));
-                    bw.write(sb.toString() + list.get(index) + "\n");
+        while(!next.isEmpty()){
+            temp = next.poll();
+            x = temp.x;
+            y = temp.y;
+            if(x == n - 1 && y == n - 1)
+                count++;
+            else{
+                if(x < n - 1 && y < n - 1){
+                    if(table[x + 1][y].equals)
                 }
-                index++;
+                if (temp.x < n - 1){
+
+                }
+                if(temp.y < n - 1){
+
+                }
             }
-        else while(index < n)
-            print(new StringBuilder(sb).append(list.get(index)).append(" "), list, index++, count + 1);
+
+        }
+
+
+        return count;
+    }
+}
+class Pipe{
+    int x, y;
+
+    public Pipe(int x, int y, int pos) {
+        this.x = x;
+        this.y = y;
+        this.pos = pos;
     }
 
+    int pos;
 }
-
 
 
 
