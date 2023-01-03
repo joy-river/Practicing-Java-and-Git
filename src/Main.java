7c4eb91c110091c85be573bf421327fbef307390
Harigaze <dlrkd1122@korea.ac.kr>
2023-01-03 17:07:29 +0900

    하는중

--- src/Main.java
+++ src/Main.java
@@ -7,7 +7,9 @@ public class Main {
 
     public static void main(String[] args) throws IOException {
         int t = Integer.parseInt(br.readLine());
-
+        for (int i = 0; i < t; i ++){
+            int n = Integer.parseInt(br.readLine());
+        }
 
 
         bw.flush();
