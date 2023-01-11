be60c24953bd2497bfd48663c9652044355c8f24
Harigaze <dlrkd1122@korea.ac.kr>
2023-01-11 17:20:28 +0900

    하는중

--- src/Main.java
+++ src/Main.java
@@ -5,23 +5,29 @@ public class Main {
     static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
     static double sum;
-
+    static int n;
     public static void main(String[] args) throws IOException {
         int t = Integer.parseInt(br.readLine());
         StringTokenizer st;
+        double a, b, xsum = 0, ysum = 0;
         for (int i = 0; i < t; i ++){
             ArrayList<vec> list = new ArrayList<>();
-            int n = Integer.parseInt(br.readLine());
+            n = Integer.parseInt(br.readLine());
             sum = Double.MAX_VALUE;
+            xsum = 0;
+            ysum = 0;
             for (int j = 0 ; j < n ; j++){
                 st = new StringTokenizer(br.readLine());
-                list.add(new vec(Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken())));
+                a = Double.parseDouble(st.nextToken());
+                b = Double.parseDouble(st.nextToken());
+                xsum += a;
+                ysum += b;
+                list.add(new vec(a, b));
             }
 
-            brute(n, new vec(0, 0) , list);
+            brute(n / 2,0, xsum, ysum, list);
 
             bw.write(sum + "\n");
-
 
         }
 
@@ -29,20 +35,15 @@ public class Main {
         bw.flush();
         bw.close();
     }
-    static void brute(int n, vec temp, ArrayList<vec> list){
-        vec a, b;
-        if(n == 0){
-            sum = Math.min(sum, temp.getLength());
-        }
+    static void brute(int count, int index, double xsum, double ysum, ArrayList<vec> list){
+        vec b;
+        if(count == 0)
+            sum = Math.min(sum, Math.sqrt(xsum * xsum + ysum * ysum));
         else{
-            a = list.remove(0);
-            for (int j = 0; j < list.size(); j++){
-                b = list.remove(0);
-                brute(n - 2, new vec(temp.x + (a.x - b.x), temp.y + (a.y - b.y)), list);
-                brute(n - 2, new vec(temp.x + (b.x - a.x), temp.y + (b.y - a.y)), list);
-                list.add(b);
+            for (int j = index; j < n ; j++){
+                b = list.get(index);
+                brute(count - 1, index + 1,xsum - 2 * b.x, ysum - 2 * b.y, list);
             }
-            list.add(a);
         }
     }
 
@@ -55,11 +56,6 @@ class vec {
         this.x = x;
         this.y = y;
     }
-
-    public double getLength(){
-        return Math.sqrt(this.x * this.x + this.y * this.y);
-    }
-
 }
 
 
