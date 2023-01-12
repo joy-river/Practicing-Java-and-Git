20c98b59a2580c49f05442b720e14bff18a7c77b
Harigaze <dlrkd1122@korea.ac.kr>
2023-01-12 09:08:11 +0900

    백준 1007번 벡터 매칭 브루트포스 문제
    일단 모든 점의 x, y를 더함. 그리고 점 갯수의 반을 뽑아서 각각의 x, y를 두배 한 값을 빼주면 벡터의 합의 길이가 나옴. 문제는 이 점 갯수의 반을 뽑는 과정에서 시간초과가 뿜어질 위험이 있음. 조합을 구현하느냐에 따라 달라질 수 있는데, 백트래킹은 계속 시간초과가 떠서 재귀로 만들었더니 통과함.

---
+++ src/BruteForce/VectorMatching.java
@@ -0,0 +1,67 @@
+package BruteForce;
+
+import java.io.*;
+import java.util.*;
+
+public class VectorMatching {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    static double sum;
+    static int n;
+    public static void main(String[] args) throws IOException {
+        int t = Integer.parseInt(br.readLine());
+        StringTokenizer st;
+        double a, b, xsum = 0, ysum = 0;
+        for (int i = 0; i < t; i ++){
+            ArrayList<vec> list = new ArrayList<>();
+            n = Integer.parseInt(br.readLine());
+            sum = Double.MAX_VALUE;
+            xsum = 0;
+            ysum = 0;
+            for (int j = 0 ; j < n ; j++){
+                st = new StringTokenizer(br.readLine());
+                a = Double.parseDouble(st.nextToken());
+                b = Double.parseDouble(st.nextToken());
+                xsum += a;
+                ysum += b;
+                list.add(new vec(a, b));
+            }
+
+            brute(n / 2,0, xsum, ysum, list);
+
+            bw.write(sum + "\n");
+
+        }
+
+
+        bw.flush();
+        bw.close();
+    }
+    static void brute(int count, int index, double xsum, double ysum, ArrayList<vec> list){
+        vec b;
+        if(count == 0)
+            sum = Math.min(sum, Math.sqrt(xsum * xsum + ysum * ysum));
+        if(index == n)
+            return;
+        else {
+            b = list.get(index);
+            brute(count - 1, index + 1, xsum - 2 * b.x, ysum - 2 * b.y, list);
+            brute(count, index + 1, xsum, ysum, list);
+        }
+
+    }
+
+}
+class vec {
+    double x;
+    double y;
+
+    public vec(double x, double y) {
+        this.x = x;
+        this.y = y;
+    }
+}
+
+
+
+
