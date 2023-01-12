49820c82b2c883dcbb6d2317e31e04bf0ffe0732
Harigaze <dlrkd1122@korea.ac.kr>
2023-01-12 17:08:21 +0900

    하는중

--- src/Main.java
+++ src/Main.java
@@ -4,60 +4,40 @@ import java.util.*;
 public class Main {
     static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-    static double sum;
-    static int n;
+
     public static void main(String[] args) throws IOException {
-        int t = Integer.parseInt(br.readLine());
-        StringTokenizer st;
-        double a, b, xsum = 0, ysum = 0;
-        for (int i = 0; i < t; i ++){
-            ArrayList<vec> list = new ArrayList<>();
-            n = Integer.parseInt(br.readLine());
-            sum = Double.MAX_VALUE;
-            xsum = 0;
-            ysum = 0;
-            for (int j = 0 ; j < n ; j++){
-                st = new StringTokenizer(br.readLine());
-                a = Double.parseDouble(st.nextToken());
-                b = Double.parseDouble(st.nextToken());
-                xsum += a;
-                ysum += b;
-                list.add(new vec(a, b));
-            }
-
-            brute(n / 2,0, xsum, ysum, list);
-
-            bw.write(sum + "\n");
-
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int k = Integer.parseInt(st.nextToken());
+        ArrayList<Jewel> Jlist = new ArrayList<>();
+        for (int i = 0 ; i < n ; i++) {
+            st = new StringTokenizer(br.readLine());
+            Jlist.add(new Jewel(Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken())));
         }
 
 
         bw.flush();
         bw.close();
     }
-    static void brute(int count, int index, double xsum, double ysum, ArrayList<vec> list){
-        vec b;
-        if(count == 0)
-            sum = Math.min(sum, Math.sqrt(xsum * xsum + ysum * ysum));
-        else{
-            for (int j = index; j < n ; j++){
-                b = list.get(index);
-                brute(count - 1, index + 1,xsum - 2 * b.x, ysum - 2 * b.y, list);
-            }
-        }
-    }
 
 }
-class vec {
-    double x;
-    double y;
+class Jewel {
+    int m, v;
 
-    public vec(double x, double y) {
-        this.x = x;
-        this.y = y;
+    public Jewel(int m, int v) {
+        this.m = m;
+        this.v = v;
     }
 }
+class Baag{
+    int carry;
+    boolean full;
 
+    public Baag(int carry, boolean full) {
+        this.carry = carry;
+        this.full = full;
+    }
+}
 
 
 
