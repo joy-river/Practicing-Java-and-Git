90d10293714e9a586412d918f2997be6c7fc380f
Harigaze <dlrkd1122@korea.ac.kr>
2023-01-13 17:12:28 +0900

    하는중

--- src/Main.java
+++ src/Main.java
@@ -4,20 +4,43 @@ import java.util.*;
 public class Main {
     static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    static long result = 0;
 
     public static void main(String[] args) throws IOException {
         StringTokenizer st = new StringTokenizer(br.readLine());
         int n = Integer.parseInt(st.nextToken());
         int k = Integer.parseInt(st.nextToken());
-        ArrayList<Jewel> Jlist = new ArrayList<>();
+        PriorityQueue<Jewel> Jlist = new PriorityQueue<>(Comparator.comparingInt(o -> o.m));
+        PriorityQueue<Integer> Blist = new PriorityQueue<>();
+
         for (int i = 0 ; i < n ; i++) {
             st = new StringTokenizer(br.readLine());
             Jlist.add(new Jewel(Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken())));
         }
+        for (int i = 0 ; i < k ; i++)
+           Blist.add(Integer.parseInt(br.readLine()));
 
+        Pick(Jlist, Blist);
 
+        bw.write(Long.toString(result));
         bw.flush();
         bw.close();
+    }
+
+
+    static void Pick (PriorityQueue<Jewel> Jlist, PriorityQueue<Integer> Blist){
+        int btemp;
+        long sum = 0;
+        PriorityQueue<Integer> next = new PriorityQueue<>(Comparator.reverseOrder());
+
+        while(!Blist.isEmpty()) {
+            btemp = Blist.poll();
+            while(!Jlist.isEmpty() && Jlist.peek().m <= btemp)
+                next.add(Jlist.poll().v);
+            if(!next.isEmpty())
+                sum += next.poll();
+        }
+        result =  sum;
     }
 
 }
@@ -29,15 +52,5 @@ class Jewel {
         this.v = v;
     }
 }
-class Baag{
-    int carry;
-    boolean full;
-
-    public Baag(int carry, boolean full) {
-        this.carry = carry;
-        this.full = full;
-    }
-}
-
 
 
