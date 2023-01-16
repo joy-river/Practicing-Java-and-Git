94f1401c6e660bb1cb79daf1c4444da2a56e0bf7
Harigaze <dlrkd1122@korea.ac.kr>
2023-01-16 17:09:02 +0900

    하는중

--- src/Main.java
+++ src/Main.java
@@ -4,53 +4,29 @@ import java.util.*;
 public class Main {
     static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-    static long result = 0;
 
     public static void main(String[] args) throws IOException {
         StringTokenizer st = new StringTokenizer(br.readLine());
         int n = Integer.parseInt(st.nextToken());
-        int k = Integer.parseInt(st.nextToken());
-        PriorityQueue<Jewel> Jlist = new PriorityQueue<>(Comparator.comparingInt(o -> o.m));
-        PriorityQueue<Integer> Blist = new PriorityQueue<>();
+        int s = Integer.parseInt(st.nextToken());
 
-        for (int i = 0 ; i < n ; i++) {
-            st = new StringTokenizer(br.readLine());
-            Jlist.add(new Jewel(Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken())));
-        }
-        for (int i = 0 ; i < k ; i++)
-           Blist.add(Integer.parseInt(br.readLine()));
+        int[] list = new int[n + 1];
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0 ; i < n ; i++)
+            list[i] = Integer.parseInt(st.nextToken());
 
-        Pick(Jlist, Blist);
 
-        bw.write(Long.toString(result));
+
+
+
         bw.flush();
         bw.close();
     }
 
 
-    static void Pick (PriorityQueue<Jewel> Jlist, PriorityQueue<Integer> Blist){
-        int btemp;
-        long sum = 0;
-        PriorityQueue<Integer> next = new PriorityQueue<>(Comparator.reverseOrder());
 
-        while(!Blist.isEmpty()) {
-            btemp = Blist.poll();
-            while(!Jlist.isEmpty() && Jlist.peek().m <= btemp)
-                next.add(Jlist.poll().v);
-            if(!next.isEmpty())
-                sum += next.poll();
-        }
-        result =  sum;
-    }
 
 }
-class Jewel {
-    int m, v;
 
-    public Jewel(int m, int v) {
-        this.m = m;
-        this.v = v;
-    }
-}
 
 
