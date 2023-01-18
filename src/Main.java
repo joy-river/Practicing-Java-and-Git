b8195a5a1f4e247f9000b3dca9684e938ae2ab3a
Harigaze <dlrkd1122@korea.ac.kr>
2023-01-18 17:10:15 +0900

    하는중

--- src/Main.java
+++ src/Main.java
@@ -6,17 +6,11 @@ public class Main {
     static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
     public static void main(String[] args) throws IOException {
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        int n = Integer.parseInt(st.nextToken());
-        int s = Integer.parseInt(st.nextToken());
-
-        int[] list = new int[n + 1];
-        st = new StringTokenizer(br.readLine());
-        for (int i = 0 ; i < n ; i++)
-            list[i] = Integer.parseInt(st.nextToken());
-
-
-
+        String[] input = br.readLine().split("");
+        int result = input.length;
+        int [] pal = new int[result + 1];
+        for (int i = 1 ; i <= pal.length ; i++ )
+            pal[i] = i;
 
 
         bw.flush();
