0cd1fed7dacfda8086f6cec64a383556da64030d
Harigaze <dlrkd1122@korea.ac.kr>
2023-01-27 17:23:48 +0900

    하는중

--- src/Main.java
+++ src/Main.java
@@ -5,49 +5,61 @@ import java.util.*;
 public class Main {
     static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-    static long mod = 1000000000;
-    static long [][][] dp = new long[101][10][1 << 10];
-    static long count = 0;
-    static int n;
+    static int n, count = 0;
+    static ArrayList<Long> prime = new ArrayList<>();
 
     public static void main(String[] args) throws IOException {
         n = Integer.parseInt(br.readLine());
-        if(n < 10)
-            bw.write(0);
-        else{
-            for (int i = 1 ; i < 10; i++) {
-                dp[1][i][1 << i] = 1;
-                Stair(1, i, 1 << i);
+        int limit = (int) Math.sqrt(n);
+        boolean [] Era = new boolean[n + 1];
+        long temp = 0, last = 0;
+        prime.add(0L);
+
+
+        for (int i = 2; i <= limit; i++){
+            if(!Era[i]){
+                for (int j = 2; i * j <= n ; j ++)
+                    Era[i * j] = true;
             }
-            bw.write(Long.toString(count));
         }
 
+        for(int i = 2 ; i <= n / 2 ; i++)
+            if(!Era[i]) {
+                if(last + i > n)
+                    break;
+                if(i + temp >= n)
+                    prime.add(i + temp);
+                temp += i;
+                last = i;
+            }
+        if(!Era[n])
+            count++;
+        Make_Sum(prime.size() - 1, prime.size() - 2);
 
+
+
+
+        bw.write(Integer.toString(count));
         bw.flush();
         bw.close();
     }
-    static void Stair(int i , int j, int k){
-        int a,b,c;
-            if(i == n) {
-                if (k == (1 << 10) - 1)
-                    count ++;// dp[i][j][k] % mod;
-            }
-            else {
-                a = i + 1;
-                if (j != 0) {
-                    b = j - 1;
-                    c = k | (1 << b);
-                    dp[a][b][c] ++;//= (dp[i][j][k] % mod);
-                    Stair(a, b, c);
-                }
-                if (j != 9) {
-                    b = j + 1;
-                    c = k | (1 << b);
-                    dp[a][b][c] ++;//= (dp[i][j][k] % mod);
-                    Stair(a, b, c);
-                }
-            }
+    static void Make_Sum(int H_index, int T_index){
+        if (T_index < 0)
+            return;
+        if (prime.get(H_index) < n)
+            return;
+        else {
+            long sum = prime.get(H_index) - prime.get(T_index);
+            if (sum == n)
+                count++;
+            if (sum <= n)
+                Make_Sum(H_index, T_index - 1);
+            else if (sum > n)
+                Make_Sum(H_index - 1, T_index);
+        }
+
     }
+
 }
 
 
