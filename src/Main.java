9dd005ac7b60d5674e4d8f25f576bdfdc3049c92
Harigaze <dlrkd1122@korea.ac.kr>
2023-02-02 17:09:30 +0900

    하는중

--- src/Main.java
+++ src/Main.java
@@ -1,54 +1,62 @@
 import java.io.*;
+import java.security.Policy;
 import java.util.*;
 
 
 public class Main {
     static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    static int Pick_Count = 0;
 
     public static void main(String[] args) throws IOException{
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        int n = Integer.parseInt(st.nextToken());
-        int m = Integer.parseInt(st.nextToken());
-        StringBuilder sb = new StringBuilder();
-        int a, b, temp;
+        int n = Integer.parseInt(br.readLine());
+        String[][] chess = new String[n][];
+        ArrayList<Bishop> Possible = new ArrayList<>();
 
-        HashSet<Integer> full = new HashSet<>();
-        for (int i = 1 ; i <= n; i++)
-            full.add(i);
-        Queue<Integer>[] link = new Queue[n + 1];
-        int [] In_Degree = new int[n + 1];
-        for (int i = 1 ; i <= n ; i ++)
-            link[i] = new LinkedList<>();
-
-
-        for (int i = 0 ; i < m ; i ++){
-            st = new StringTokenizer(br.readLine());
-            a = Integer.parseInt(st.nextToken());
-            b = Integer.parseInt(st.nextToken());
-
-            link[a].add(b);
-            In_Degree[b]++;
-            full.remove(b);
-        }
-        PriorityQueue<Integer> next = new PriorityQueue<>(full);
-
-        while(!next.isEmpty()){
-            temp = next.poll();
-            while(!link[temp].isEmpty()){
-                a = link[temp].poll();
-                if(--In_Degree[a] == 0)
-                    next.add(a);
-            }
-            sb.append(temp).append(" ");
+        for (int i = 0; i < n ; i ++) {
+            chess[i] = br.readLine().split(" ");
+            for (int j = 0 ; j < n ; j++)
+                if(chess[i][j].equals("1"))
+                    Possible.add(new Bishop(i + j, i - j - n));
         }
 
-        bw.write(sb.toString());
+            Fill_Chess(new HashSet<>(), Possible,0, 0);
+
+
+
+
+        bw.write(Integer.toString(Pick_Count));
         bw.flush();
         bw.close();
     }
 
+    static void Fill_Chess(HashSet<Integer> picked, ArrayList<Bishop> list,int index, int count){
+        if(index == list.size())
+            Pick_Count = Math.max(Pick_Count, count);
+        else if (count + list.size() - index < Pick_Count)
+            return;
+        else {
+            Bishop temp = list.get(index);
+            if (!picked.contains(temp.DR) && !picked.contains(temp.UR)) {
+                picked.add(temp.DR);
+                picked.add(temp.UR);
+                Fill_Chess(picked, list, index + 1, count + 1);
+                picked.remove(temp.DR);
+                picked.remove(temp.UR);
+            }
+             Fill_Chess(picked, list, index + 1, count);
+        }
 
+    }
+
+}
+class Bishop{
+    int UR, DR;
+
+    public Bishop(int UR, int DR) {
+        this.UR = UR;
+        this.DR = DR;
+    }
 }
 
 
