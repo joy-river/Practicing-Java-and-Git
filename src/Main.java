d23a7042bfd8b6f26e20bc47e46f5c6c71a1d5c9
Harigaze <dlrkd1122@korea.ac.kr>
2023-02-08 17:06:28 +0900

    하는중

--- src/Main.java
+++ src/Main.java
@@ -1,67 +1,44 @@
+import DFSandBFS.UnionFind;
+
 import java.io.*;
-import java.security.Policy;
 import java.util.*;
 
 
 public class Main {
     static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-    static int Pick_Count = 0;
 
     public static void main(String[] args) throws IOException{
         int n = Integer.parseInt(br.readLine());
-        String[][] chess = new String[n][];
-        ArrayList<Bishop> Possible = new ArrayList<>();
+        String[][] input = new String[n][];
+        StringTokenizer st;
+        ArrayList<Line> lines = new ArrayList<>();
 
-        for (int i = 0; i < n ; i ++) {
-            chess[i] = br.readLine().split(" ");
-            for (int j = 0 ; j < n ; j++)
-                if(chess[i][j].equals("1"))
-                    Possible.add(new Bishop(i + j, i - j - n));
+        for (int i = 0 ; i < n ; i ++) {
+            st = new StringTokenizer(br.readLine());
+            lines.add(new Line(Integer.parseInt(st.nextToken()),Integer.parseInt(st.nextToken()),Integer.parseInt(st.nextToken()),Integer.parseInt(st.nextToken())));
         }
 
-            Fill_Chess(new HashSet<>(), Possible,0, 0);
 
 
-
-
-        bw.write(Integer.toString(Pick_Count));
         bw.flush();
         bw.close();
     }
 
-    static void Fill_Chess(HashSet<Integer> picked, ArrayList<Bishop> list,int index, int count){
-        if(index == list.size())
-            Pick_Count = Math.max(Pick_Count, count);
-        else if (count + list.size() - index < Pick_Count)
-            return;
-        else {
-            Bishop temp = list.get(index);
-            if (!picked.contains(temp.DR) && !picked.contains(temp.UR)) {
-                picked.add(temp.DR);
-                picked.add(temp.UR);
-                Fill_Chess(picked, list, index + 1, count + 1);
-                picked.remove(temp.DR);
-                picked.remove(temp.UR);
-            }
-             Fill_Chess(picked, list, index + 1, count);
-        }
-
+    static boolean Meet (Line a, Line b){
+        return (a.x1 - a.x2)*(b.y1 - b.y2) - (a.y1 - a.y2)*(b.x1 - b.x2) == 0;
     }
 
 }
-class Bishop{
-    int UR, DR;
 
-    public Bishop(int UR, int DR) {
-        this.UR = UR;
-        this.DR = DR;
+class Line{
+    int x1, y1;
+    int x2, y2;
+
+    public Line(int x1, int y1, int x2, int y2) {
+        this.x1 = x1;
+        this.y1 = y1;
+        this.x2 = x2;
+        this.y2 = y2;
     }
 }
-
-
-
-
-
-
-
