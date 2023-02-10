154dd1c3e9938eee10e8789b8679378d80a81877
Harigaze <dlrkd1122@korea.ac.kr>
2023-02-10 16:01:19 +0900

    백준 2166번 다각형의 면적 기하학 문제(신발끈)
    다각형이 어떤 다각형인지 말한 적 없음 -> 오목한 다각형일 수 있음(삼각형으로 분할 못시키는 다각형). 이건 너무한거 아니냐고.

---
+++ src/Geometry/PloygonArea.java
@@ -0,0 +1,37 @@
+package Geometry;
+
+import java.io.*;
+import java.util.*;
+
+
+public class PloygonArea {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+    public static void main(String[] args) throws IOException {
+        int n = Integer.parseInt(br.readLine());
+        StringTokenizer st;
+        double[]x = new double[n];
+        double[]y = new double[n];
+        double sum = 0;
+
+        for (int i = 0 ; i < n ; i++){
+            st = new StringTokenizer(br.readLine());
+            x[i] = Integer.parseInt(st.nextToken());
+            y[i] = Integer.parseInt(st.nextToken());
+        }
+        for (int i = 0 ; i < n - 1 ; i ++)
+            sum +=  x[i]*y[i + 1] - x[1 + i]*y[i];
+        sum += x[n - 1]*y[0] - x[0]* y[n - 1];
+
+
+        bw.write(String.format("%.1f", Math.abs(sum) / 2));
+        bw.flush();
+        bw.close();
+    }
+
+
+
+
+}
+
