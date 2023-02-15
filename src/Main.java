fe38e423878c64744f1b807736f9ef84ec6db832
Harigaze <dlrkd1122@korea.ac.kr>
2023-02-15 17:15:07 +0900

    하는중

--- src/Main.java
+++ src/Main.java
@@ -8,46 +8,54 @@ public class Main {
 
     public static void main(String[] args) throws IOException {
         StringTokenizer st = new StringTokenizer(br.readLine());
+        StringBuilder sb = new StringBuilder();
         int n = Integer.parseInt(st.nextToken());
         int m = Integer.parseInt(st.nextToken());
-        int a, b;
-
-        int [] Deg = new int[n + 1];
+        int[] In_Degree = new int[n + 1];
         Queue<Integer>[] link = new Queue[n + 1];
-        HashSet<Integer> Full = new HashSet<>();
+        HashSet<Integer> full_set = new HashSet<>();
 
         for (int i = 1; i <= n ; i++) {
             link[i] = new LinkedList<>();
-            Full.add(i);
+            full_set.add(i);
         }
 
-        for (int i = 0 ; i < m; i ++){
+        int u, a, b;
+
+        for (int i = 0; i < m ; i++){
             st = new StringTokenizer(br.readLine());
-            a = Integer.parseInt(st.nextToken());
+            u = Integer.parseInt(st.nextToken());
             b = Integer.parseInt(st.nextToken());
+            for (int j = 0; j < u - 1 ; j++){
+                a = b;
+                b = Integer.parseInt(st.nextToken());
 
-            link[a].add(b);
-            Deg[b]++;
-            Full.remove(b);
+                In_Degree[b]++;
+                link[a].add(b);
+                full_set.remove(b);
+            }
         }
-        PriorityQueue<Integer> next = new PriorityQueue<>(Full);
+        PriorityQueue<Integer> next = new PriorityQueue<>(full_set);
 
         while(!next.isEmpty()){
             a = next.poll();
-            bw.write(a + " ");
-            while(!link[a].isEmpty()) {
-                if (--Deg[b = link[a].poll()] == 0)
+            full_set.add(a);
+            sb.append(a + "\n");
+
+            while(!link[a].isEmpty()){
+                b = link[a].poll();
+                if(--In_Degree[b] == 0)
                     next.add(b);
             }
+
         }
 
+        bw.write(full_set.size() != n ? "0" :  sb.toString());
         bw.flush();
         bw.close();
 
 
     }
-
-
 
 }
 
