26ed11d65c7784efb7b10e516135766299a60c44
Harigaze <dlrkd1122@korea.ac.kr>
2023-02-21 17:08:11 +0900

    하는중

--- src/Main.java
+++ src/Main.java
@@ -8,11 +8,50 @@ public class Main {
     public static void main(String[] args) throws IOException {
         int n = Integer.parseInt(br.readLine());
 
-
+        for (int i = 0 ; i < n ; i ++)
+            bw.write(Term_Project() + "\n");
 
 
         bw.flush();
         bw.close();
     }
+    static HashSet<Integer> team;
+    static HashSet<Integer> Not_team;
+    static int [] link;
+
+    static int Term_Project() throws IOException{
+        int n = Integer.parseInt(br.readLine());
+        int result = 0;
+
+        team = new HashSet<>();
+        Not_team = new HashSet<>();
+        link = new int[n + 1];
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        for (int i = 1 ; i <= n ; i++)
+            link[i] = Integer.parseInt(st.nextToken());
+
+        return result;
+    }
+
+    static void Is_Cycle(int a){
+       HashSet<Integer> Appeared = new HashSet<>(a);
+       Queue<Integer> save = new LinkedList<>();
+       save.add(a);
+       a = link[a];
+       while(!Appeared.contains(a)){
+           Appeared.add(a);
+           save.add(a);
+           a = link[a];
+       }
+
+       while(!)
+
+
+
+    }
+
+
 
 }
