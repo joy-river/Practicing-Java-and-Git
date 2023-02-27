7216494804ba4295d1e12f3bea2bc895ff3a93dc
Harigaze <dlrkd1122@korea.ac.kr>
2023-02-27 15:27:52 +0900

    백준 13459번 구슬 탈출 구현(bfs) 문제
    저번 구슬 탈출 2를 엄청 개선했음!!!!
    
    x y좌표로 1씩 움직이는 방법을 배웠다!

--- src/Simulation/BallEscape.java
+++ src/Simulation/BallEscape.java
@@ -6,250 +6,125 @@ import java.util.*;
 public class BallEscape {
     static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-    static int n, m, result = -1;
+    static int n, m, result = 0;
     static String[][] board;
-    static Ball_Index Red, Blue, Hole;
-    static Queue<Ball_Index> Move = new LinkedList<>();
-    static ArrayList<Ball_Index> A_Move = new ArrayList<>();
+    static boolean[][][][] visited;
+    static int[] X_Move = {1, 0, -1, 0};
+    static int[] Y_Move = {0, 1, 0, -1};
+    static Ball_Index Red, Blue;
 
     public static void main(String[] args) throws IOException {
         StringTokenizer st = new StringTokenizer(br.readLine());
         n = Integer.parseInt(st.nextToken());
         m = Integer.parseInt(st.nextToken());
         board = new String[n][m];
-        Ball_Set temp, in = new Ball_Set(0, 0, null , null);
+        visited = new boolean[n][m][n][m];
+        Ball_Set temp;
+        boolean R_hole, B_hole;
         PriorityQueue<Ball_Set> next = new PriorityQueue<>(Comparator.comparingInt(o -> o.count));
 
-        for (int i = 0 ; i < n ; i++)
+        for (int i = 0; i < n; i++)
             board[i] = br.readLine().split("");
 
-        for (int i = 0 ; i < n ; i++)
-            for (int j = 0 ; j < m ; j++){
-                if(board[i][j].equals("O"))
-                    Hole = new Ball_Index(i , j, "O");
-                if(board[i][j].equals("R")) {
-                    Red = new Ball_Index(i, j, "R");
-                    board[i][j] = ".";
-                }
-                if(board[i][j].equals("B")) {
-                    Blue = new Ball_Index(i, j, "B");
-                    board[i][j] = ".";
-                }
+        for (int i = 0; i < n; i++)
+            for (int j = 0; j < m; j++) {
+                if (board[i][j].equals("R"))
+                    Red = new Ball_Index(i, j);
+                if (board[i][j].equals("B"))
+                    Blue = new Ball_Index(i, j);
             }
 
-        next.add(new Ball_Set(0, 0, Red, Blue));
+        next.add(new Ball_Set(1, Red, Blue));
 
-        while(!next.isEmpty()){
+        while (result == 0 && !next.isEmpty()) {
             temp = next.poll();
-            if(temp.count > 10)
+            visited[Red.x][Red.y][Blue.x][Blue.y] = true;
+
+            if (temp.count > 10)
                 break;
 
-            if(temp.a.compare(Hole)) {
-                if(temp.b.compare(Hole))
+            for (int i = 0; i < 4; i++) {
+                Red = new Ball_Index(temp.a.x, temp.a.y);
+                Blue = new Ball_Index(temp.b.x, temp.b.y);
+                R_hole = false;
+                B_hole = false;
+
+                while (!board[Red.x + X_Move[i]][Red.y + Y_Move[i]].equals("#")) {
+                    Red.x += X_Move[i];
+                    Red.y += Y_Move[i];
+
+                    if (board[Red.x][Red.y].equals("O")) {
+                        R_hole = true;
+                        break;
+                    }
+                }
+
+                while (!board[Blue.x + X_Move[i]][Blue.y + Y_Move[i]].equals("#")) {
+                    Blue.x += X_Move[i];
+                    Blue.y += Y_Move[i];
+
+                    if (board[Blue.x][Blue.y].equals("O")) {
+                        B_hole = true;
+                        break;
+                    }
+                }
+
+                if (B_hole)
                     continue;
-                else{
+                else if (R_hole) {
                     result = temp.count;
                     break;
                 }
-            }
-            if(temp.b.compare(Hole))
-                continue;
 
-            if(temp.dir != 2 && Move_Left(temp, in))
-                next.add(new Ball_Set(in.count,in.dir, in.a, in.b));
-            if(temp.dir != 1 && Move_Right(temp, in))
-                next.add(new Ball_Set(in.count, in.dir,  in.a, in.b));
-            if(temp.dir != 3 && Move_Down(temp, in))
-                next.add(new Ball_Set(in.count ,in.dir, in.a, in.b));
-            if(temp.dir != 4 && Move_Up(temp, in))
-                next.add(new Ball_Set(in.count ,in.dir,  in.a, in.b));
+                if (Red.compare(Blue)) {
+                    if (i == 0)
+                        if (temp.a.x > temp.b.x) Blue.x--;
+                        else Red.x--;
+                    if (i == 1)
+                        if (temp.a.y > temp.b.y) Blue.y--;
+                        else Red.y--;
+                    if (i == 2)
+                        if (temp.a.x > temp.b.x) Red.x++;
+                        else Blue.x++;
+                    if (i == 3)
+                        if (temp.a.y > temp.b.y) Red.y++;
+                        else Blue.y++;
+                }
+
+                if (!visited[Red.x][Red.y][Blue.x][Blue.y])
+                    next.add(new Ball_Set(temp.count + 1, new Ball_Index(Red.x, Red.y), new Ball_Index(Blue.x, Blue.y)));
+            }
+
         }
 
-        bw.write(Integer.toString(result));
+        bw.write(result == 0 ? "0" : "1");
         bw.flush();
         bw.close();
 
     }
-    static boolean Move_Left(Ball_Set temp, Ball_Set in){
-        if(temp.a.y < temp.b.y) {
-            Move.add(temp.a);
-            Move.add(temp.b);
-        }
-        else{
-            Move.add(temp.b);
-            Move.add(temp.a);
-        }
-        Ball_Index a;
-
-        while(!Move.isEmpty()){
-            a = Move.poll();
-            for (int i = a.y - 1; i >= 0 ;i--){
-                if(board[a.x][i].equals("#")) {
-                    A_Move.add(new Ball_Index(a.x, i + 1, a.color));
-                    break;
-                }
-                if(board[a.x][i].equals("O")){
-                    A_Move.add(new Ball_Index(a.x, i, a.color));
-                    break;
-                }
-                if(!A_Move.isEmpty())
-                    if(A_Move.get(0).x == a.x && A_Move.get(0).y == i) {
-                        A_Move.add(new Ball_Index(a.x, i + 1, a.color));
-                        break;
-                    }
-            }
-        }
-        in.count = temp.count + 1;
-        in.dir = 1;
-        in.a = A_Move.get(0).color.equals("R") ? A_Move.get(0) : A_Move.get(1);
-        in.b = A_Move.get(1).color.equals("B") ? A_Move.get(1) : A_Move.get(0);
-        A_Move.clear();
-
-        return !in.a.compare(temp.a) || !in.b.compare(temp.b);
-    }
-
-    static boolean Move_Right(Ball_Set temp, Ball_Set in){
-        if(temp.a.y > temp.b.y) {
-            Move.add(temp.a);
-            Move.add(temp.b);
-        }
-        else{
-            Move.add(temp.b);
-            Move.add(temp.a);
-        }
-        Ball_Index a;
-
-        while(!Move.isEmpty()){
-            a = Move.poll();
-            for (int i = a.y + 1; i < m ; i++){
-                if(board[a.x][i].equals("#")) {
-                    A_Move.add(new Ball_Index(a.x, i - 1, a.color));
-                    break;
-                }
-                if(board[a.x][i].equals("O")){
-                    A_Move.add(new Ball_Index(a.x, i, a.color));
-                    break;
-                }
-                if(!A_Move.isEmpty())
-                    if(A_Move.get(0).x == a.x && A_Move.get(0).y == i) {
-                        A_Move.add(new Ball_Index(a.x, i - 1, a.color));
-                        break;
-                    }
-            }
-        }
-        in.count = temp.count + 1;
-        in.dir = 2;
-        in.a = A_Move.get(0).color.equals("R") ? A_Move.get(0) : A_Move.get(1);
-        in.b = A_Move.get(1).color.equals("B") ? A_Move.get(1) : A_Move.get(0);
-        A_Move.clear();
-
-        return !in.a.compare(temp.a) || !in.b.compare(temp.b);
-    }
-
-    static boolean Move_Up(Ball_Set temp, Ball_Set in){
-        if(temp.a.x < temp.b.x) {
-            Move.add(temp.a);
-            Move.add(temp.b);
-        }
-        else{
-            Move.add(temp.b);
-            Move.add(temp.a);
-        }
-        Ball_Index a;
-
-        while(!Move.isEmpty()){
-            a = Move.poll();
-            for (int i = a.x - 1; i >= 0 ;i--){
-                if(board[i][a.y].equals("#")) {
-                    A_Move.add(new Ball_Index(i + 1, a.y, a.color));
-                    break;
-                }
-                if(board[i][a.y].equals("O")){
-                    A_Move.add(new Ball_Index(i, a.y, a.color));
-                    break;
-                }
-                if(!A_Move.isEmpty())
-                    if(A_Move.get(0).x == i && A_Move.get(0).y == a.y) {
-                        A_Move.add(new Ball_Index(i + 1, a.y, a.color));
-                        break;
-                    }
-            }
-        }
-
-        in.count = temp.count + 1;
-        in.dir = 3;
-        in.a = A_Move.get(0).color.equals("R") ? A_Move.get(0) : A_Move.get(1);
-        in.b = A_Move.get(1).color.equals("B") ? A_Move.get(1) : A_Move.get(0);
-        A_Move.clear();
-
-        return !in.a.compare(temp.a) || !in.b.compare(temp.b);
-    }
-
-    static boolean Move_Down(Ball_Set temp, Ball_Set in){
-        if(temp.a.x > temp.b.x) {
-            Move.add(temp.a);
-            Move.add(temp.b);
-        }
-        else{
-            Move.add(temp.b);
-            Move.add(temp.a);
-        }
-        Ball_Index a;
-
-        while(!Move.isEmpty()){
-            a = Move.poll();
-            for (int i = a.x + 1; i < n ;i++){
-                if(board[i][a.y].equals("#")) {
-                    A_Move.add(new Ball_Index(i - 1, a.y, a.color));
-                    break;
-                }
-                if(board[i][a.y].equals("O")){
-                    A_Move.add(new Ball_Index(i, a.y, a.color));
-                    break;
-                }
-                if(!A_Move.isEmpty())
-                    if(A_Move.get(0).x == i && A_Move.get(0).y == a.y) {
-                        A_Move.add(new Ball_Index(i - 1, a.y, a.color));
-                        break;
-                    }
-            }
-        }
-
-        in.count = temp.count + 1;
-        in.dir = 4;
-        in.a = A_Move.get(0).color.equals("R") ? A_Move.get(0) : A_Move.get(1);
-        in.b = A_Move.get(1).color.equals("B") ? A_Move.get(1) : A_Move.get(0);
-        A_Move.clear();
-
-        return !in.a.compare(temp.a) || !in.b.compare(temp.b);
-    }
-
 
     static class Ball_Set {
-        int count, dir;
+        int count;
         Ball_Index a, b;
 
-        public Ball_Set(int count, int dir, Ball_Index a, Ball_Index b) {
+        public Ball_Set(int count, Ball_Index a, Ball_Index b) {
             this.count = count;
-            this.dir = dir;
             this.a = a;
             this.b = b;
         }
     }
+
     static class Ball_Index {
         int x, y;
-        String color;
 
-        public Ball_Index(int x, int y, String color) {
+        public Ball_Index(int x, int y) {
             this.x = x;
             this.y = y;
-            this.color = color;
         }
 
-        public boolean compare(Ball_Index a){
+        public boolean compare(Ball_Index a) {
             return (a.x == this.x && a.y == this.y);
         }
+
     }
-
-
 }
