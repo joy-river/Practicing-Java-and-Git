9fe7c5ea9ad54139e1dba62d2e8f9e6340446cc1
Harigaze <dlrkd1122@korea.ac.kr>
2023-03-02 16:06:31 +0900

    백준 12738번 가장 긴 증가하는 부분 수열 3 LIS
    마 바이너리로 후딱 해치워라!

---
+++ src/DP/LIS3.java
@@ -0,0 +1,63 @@
+package DP;
+
+import java.io.*;
+import java.util.*;
+
+public class LIS3 {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+    public static void main(String[] args) throws IOException {
+        int n = Integer.parseInt(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int[] arr = new int[n];
+        int[] dp = new int[n];
+        List<Lis> bdp = new ArrayList<>();
+        Stack <Integer> result = new Stack<>();
+        Lis temp;
+
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+            dp[i] = 1;
+        }
+        bdp.add(new Lis(-1, null));
+        bdp.add(new Lis(arr[0], null));
+        for (int i = 1; i < n ; i ++){
+            dp[i] = dpsearch(arr[i], bdp, 1 , bdp.size() - 1);
+        }
+        bw.write(bdp.size() - 1 + "\n");
+
+
+        bw.flush();
+        bw.close();
+    }
+    static int dpsearch(int a, List<Lis> bdp, int start, int end){
+        int mid = (start + end) / 2;
+        if(start > end){
+            if(start == bdp.size())
+                bdp.add(new Lis(a, bdp.get(bdp.size() - 1)));
+            else
+            if(bdp.get(start).num > a)
+                bdp.set(start, new Lis(a, bdp.get(start - 1)));
+            return start;
+        }
+
+        if(bdp.get(mid).num > a)
+            return dpsearch(a, bdp , start, mid - 1);
+        else if(bdp.get(mid).num < a)
+            return dpsearch(a, bdp, mid + 1, end);
+        else
+            return mid;
+    }
+    static class Lis {
+        int num;
+        Lis parent;
+
+        public Lis(int num, Lis parent) {
+            this.num = num;
+            this.parent = parent;
+        }
+    }
+}
+
+
