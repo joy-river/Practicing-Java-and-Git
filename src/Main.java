3f47b2ee133efa88cc4a89d957560aa47d2b2028
Harigaze <dlrkd1122@korea.ac.kr>
2023-03-03 17:05:33 +0900

    하는중

--- src/Main.java
+++ src/Main.java
@@ -6,62 +6,48 @@ public class Main {
     static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
     public static void main(String[] args) throws IOException {
-        int n = Integer.parseInt(br.readLine());
         StringTokenizer st = new StringTokenizer(br.readLine());
-        int[] arr = new int[n];
-        int[] dp = new int[n];
-        List<Lis> bdp = new ArrayList<>();
-        Stack <Integer> result = new Stack<>();
-        Lis temp;
+        ArrayList<Integer> dp = new ArrayList<>();
+        HashSet<Integer> feet = new HashSet<>();
+        dp.add(0);
+        int temp = Integer.parseInt(st.nextToken());
+        dp.add(2);
+        feet.add(0);
+        feet.add(temp);
+        while((temp = Integer.parseInt(st.nextToken())) != 0) {
+            if (feet.contains(temp))
+                dp.add(dp.get(dp.size() - 1) + 1);
+            else {
+                if(feet.contains(0)){
+                    dp.add(dp.get(dp.size() - 1) + 2);
+                    feet.remove(0);
+                }
+                else if (feet.contains(temp == 4 ? 1 : temp + 1 )) {
+                    dp.add(dp.get(dp.size() - 1) + 3);
+                    feet.remove(temp == 4 ? 1 : temp + 1);
+                } else if (feet.contains(temp == 1 ? 4 : temp - 1)) {
+                    dp.add(dp.get(dp.size() - 1) + 3);
+                    feet.remove(temp == 1 ? 4 : temp - 1);
+                } else {
+                    dp.add(dp.get(dp.size() - 1) + 4);
+                    if (temp <= 2)
+                        feet.remove(temp + 2);
+                    else
+                        feet.remove(temp - 2);
+                }
+                feet.add(temp);
+            }
+        }
 
-        for (int i = 0; i < n; i++) {
-            arr[i] = Integer.parseInt(st.nextToken());
-            dp[i] = 1;
-        }
-        bdp.add(new Lis(-1, null));
-        bdp.add(new Lis(arr[0], null));
-        for (int i = 1; i < n ; i ++){
-            dp[i] = dpsearch(arr[i], bdp, 1 , bdp.size() - 1);
-        }
-        bw.write(bdp.size() - 1 + "\n");
-        temp = bdp.get(bdp.size() - 1);
-        for (int i = 1 ; i < bdp.size() ; i++){
-            result.add(temp.num);
-            temp = temp.parent;
-        }
-        while(!result.isEmpty())
-            bw.write(result.pop() + " ");
 
+
+
+        bw.write(Integer.toString(dp.get(dp.size() - 1)));
         bw.flush();
         bw.close();
     }
-    static int dpsearch(int a, List<Lis> bdp, int start, int end){
-        int mid = (start + end) / 2;
-        if(start > end){
-            if(start == bdp.size())
-                bdp.add(new Lis(a, bdp.get(bdp.size() - 1)));
-            else
-            if(bdp.get(start).num > a)
-                bdp.set(start, new Lis(a, bdp.get(start - 1)));
-            return start;
-        }
 
-        if(bdp.get(mid).num > a)
-            return dpsearch(a, bdp , start, mid - 1);
-        else if(bdp.get(mid).num < a)
-            return dpsearch(a, bdp, mid + 1, end);
-        else
-            return mid;
-    }
-    static class Lis {
-        int num;
-        Lis parent;
 
-        public Lis(int num, Lis parent) {
-            this.num = num;
-            this.parent = parent;
-        }
-    }
 }
 
 
