235bdbbb23e0ed2ec9a43953d23e683e1b4dcda3
Harigaze <dlrkd1122@korea.ac.kr>
2023-03-06 17:10:02 +0900

    하는중

--- src/Main.java
+++ src/Main.java
@@ -4,47 +4,51 @@ import java.util.*;
 public class Main {
     static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    static String[] step;
+    static int result = 500000;
+    static int[][][] dp;
 
     public static void main(String[] args) throws IOException {
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        ArrayList<Integer> dp = new ArrayList<>();
-        HashSet<Integer> feet = new HashSet<>();
-        dp.add(0);
-        int temp = Integer.parseInt(st.nextToken());
-        dp.add(2);
-        feet.add(0);
-        feet.add(temp);
-        while((temp = Integer.parseInt(st.nextToken())) != 0) {
-            if (feet.contains(temp))
-                dp.add(dp.get(dp.size() - 1) + 1);
-            else {
-                if(feet.contains(0)){
-                    dp.add(dp.get(dp.size() - 1) + 2);
-                    feet.remove(0);
+        step = br.readLine().split(" ");
+        int n = step.length;
+        dp = new int[n + 1][5][5];
+        for (int i = 0 ; i <= n ; i++)
+            for(int j = 0 ; j < 5 ; j++)
+                for(int s = 0 ; s < 5 ; s++)
+                    dp[i][j][s] = 500000;
+
+        dp[0][0][0] = 0;
+        int temp;
+
+        for (int i = 0 ; i < n; i++){
+            temp = Integer.parseInt(step[i]);
+            for (int j = 0 ; j < 5; j ++)
+                for(int s = 0 ; s < 5 ; s++){
+                    dp[i + 1][temp][s] = Math.min(dp[i + 1][temp][s], dp[i][j][s] + Move_cost(j, temp));
+                    dp[i + 1][j][temp] = Math.min(dp[i + 1][j][temp], dp[i][j][s] + Move_cost(s, temp));
                 }
-                else if (feet.contains(temp == 4 ? 1 : temp + 1 )) {
-                    dp.add(dp.get(dp.size() - 1) + 3);
-                    feet.remove(temp == 4 ? 1 : temp + 1);
-                } else if (feet.contains(temp == 1 ? 4 : temp - 1)) {
-                    dp.add(dp.get(dp.size() - 1) + 3);
-                    feet.remove(temp == 1 ? 4 : temp - 1);
-                } else {
-                    dp.add(dp.get(dp.size() - 1) + 4);
-                    if (temp <= 2)
-                        feet.remove(temp + 2);
-                    else
-                        feet.remove(temp - 2);
-                }
-                feet.add(temp);
-            }
         }
 
+        for (int i = 0 ; i < 5 ; i++)
+            for (int j = 0 ; j < 5 ; j++)
+                result = Math.min(result, dp[n][i][j]);
 
-
-
-        bw.write(Integer.toString(dp.get(dp.size() - 1)));
+        bw.write(Integer.toString(result));
         bw.flush();
         bw.close();
+    }
+
+    static int Move_cost(int from, int to){
+        if(to == 0)
+            return 0;
+        if(from == 0)
+            return 2;
+        if(from == to)
+            return 1;
+        if(Math.abs(from - to) == 2)
+            return 4;
+        else
+            return 3;
     }
 
 
