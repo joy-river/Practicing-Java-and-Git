02a6e1b580415ea86589e69c354ba0a45d548564
Harigaze <dlrkd1122@korea.ac.kr>
2023-05-25 14:50:26 +0900

    자바 끝

--- src/Main.java
+++ src/Main.java
@@ -4,54 +4,35 @@ import java.util.*;
 public class Main {
     static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-    static String[] step;
-    static int result = 500000;
-    static int[][][] dp;
+    static int n;
+    static String[] card;
+    static String[] score;
 
     public static void main(String[] args) throws IOException {
-        step = br.readLine().split(" ");
-        int n = step.length;
-        dp = new int[n + 1][5][5];
-        for (int i = 0 ; i <= n ; i++)
-            for(int j = 0 ; j < 5 ; j++)
-                for(int s = 0 ; s < 5 ; s++)
-                    dp[i][j][s] = 500000;
+        n = Integer.parseInt(br.readLine());
+        card = br.readLine().split("");
+        score = br.readLine().split(" ");
+        StringTokenizer st;
 
-        dp[0][0][0] = 0;
-        int temp;
+        int t = Integer.parseInt(br.readLine()), l, r;
 
-        for (int i = 0 ; i < n; i++){
-            temp = Integer.parseInt(step[i]);
-            for (int j = 0 ; j < 5; j ++)
-                for(int s = 0 ; s < 5 ; s++){
-                    dp[i + 1][temp][s] = Math.min(dp[i + 1][temp][s], dp[i][j][s] + Move_cost(j, temp));
-                    dp[i + 1][j][temp] = Math.min(dp[i + 1][j][temp], dp[i][j][s] + Move_cost(s, temp));
-                }
+        for (int i = 0; i < t; i++) {
+            st = new StringTokenizer(br.readLine());
+            l = Integer.parseInt(st.nextToken()) - 1;
+            r = Integer.parseInt(st.nextToken()) - 1;
+
+
+
         }
 
-        for (int i = 0 ; i < 5 ; i++)
-            for (int j = 0 ; j < 5 ; j++)
-                result = Math.min(result, dp[n][i][j]);
-
-        bw.write(Integer.toString(result));
         bw.flush();
         bw.close();
     }
 
-    static int Move_cost(int from, int to){
-        if(to == 0)
-            return 0;
-        if(from == 0)
-            return 2;
-        if(from == to)
-            return 1;
-        if(Math.abs(from - to) == 2)
-            return 4;
-        else
-            return 3;
-    }
-
-
 }
+
+
+
+
 
 
